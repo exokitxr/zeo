4d91cda34cf0f888e7e15869b32d5e249d5e6242
Adrian Biedrzycki <a@modules.io>
2016-11-04 01:13:08 -0700

    Implement basic plugin building

--- examples/index.js
+++ examples/index.js
@@ -10,6 +10,15 @@ const app = express();
 app.use('/', express.static(__dirname));
 
 const a = archae();
+
+a.addPlugin('react', err => {
+  if (!err) {
+    console.log('react plugin add ok');
+  } else {
+    console.warn('react plugin add fail', err);
+  }
+});
+
 a.listen({server, app});
 
 server.listen(8000);
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const fs = require('fs');
+const child_process = require('child_process');
 
 const express = require('express');
 const ws = require('ws');
+const mkdirp = require('mkdirp');
 
 class ArchaeServer {
   constructor(options) {
@@ -10,11 +13,69 @@ class ArchaeServer {
     this._options = options;
   }
 
-  add(moduleName, cb) {
-    cb(); // XXX
+  addPlugin(plugin, opts, cb) {
+    if (cb === undefined) {
+      cb = opts;
+      opts = {};
+    }
+
+    mkdirp(path.join(__dirname, 'plugins'), err => {
+      if (!err) {
+        const pluginBuildPath = path.join(__dirname, 'plugins', 'build', plugin + '.js');
+        fs.exists(pluginBuildPath, exists => {
+          if (!exists || opts.force) {
+            const yarnAdd = child_process.spawn(
+              // path.join(__dirname, 'node_modules', 'yarn', 'bin', 'yarn'),
+              'yarn',
+              [ 'add', plugin ],
+              {
+                cwd: path.join(__dirname, 'plugins'),
+              }
+            );
+            yarnAdd.stdout.pipe(process.stdout);
+            yarnAdd.stderr.pipe(process.stderr);
+            yarnAdd.on('close', code => {
+              if (code === 0) {
+                const pluginPath = path.join(__dirname, 'plugins', 'node_modules', plugin);
+
+                const webpack = child_process.spawn(
+                  path.join(__dirname, 'node_modules', 'webpack', 'bin', 'webpack.js'),
+                  [ pluginPath, pluginBuildPath ],
+                  {
+                    cwd: __dirname,
+                  }
+                );
+                webpack.stdout.pipe(process.stdout);
+                webpack.stderr.pipe(process.stderr);
+                webpack.on('close', code => {
+                  if (code === 0) {
+                    cb();
+                  } else {
+                    const err = new Error('webpack error: ' + code);
+                    cb(err);
+                  }
+                });
+              } else {
+                const err = new Error('yard add error: ' + code);
+                cb(err);
+              }
+            });
+          } else {
+            cb();
+          }
+        });
+      } else {
+        console.warn(err);
+      }
+    });
   }
   
-  remove(moduleName, cb) {
+  removePlugin(plugin, opts, cb) {
+    if (cb === undefined) {
+      cb = opts;
+      opts = {};
+    }
+
     cb(); // XXX
   }
 
