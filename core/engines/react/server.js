ffb436c6c504258c7ba41a361df6ff50f32d9d17
Adrian Biedrzycki <a@modules.io>
2016-11-05 04:27:32 -0700

    Implementing backend module loading

---
+++ core/engines/react/server.js
@@ -0,0 +1,12 @@
+console.log('load react server');
+
+const server = {
+  mount() {
+    console.log('mount react server');
+  },
+  unmount() {
+    console.log('unmount react server');
+  },
+};
+
+module.exports = server;
