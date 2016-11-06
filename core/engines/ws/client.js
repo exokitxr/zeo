599198e80142988a7ecf31ebaae1bbad9d52ae30
Adrian Biedrzycki <a@modules.io>
2016-11-06 14:43:02 -0800

    Clean up core ws engine

--- core/engines/ws/client.js
+++ core/engines/ws/client.js
@@ -1,28 +1,9 @@
 const events = require('events');
 const {EventEmitter} = events;
 
-const client = () => {
-  let connection = null;
-  let queue = [];
-
-  const result = new EventEmitter();
-
-  const _emit = result.emit;
-  result.emit = function(event, data) {
-    const o = {
-      event,
-      data,
-    };
-
-    if (connection.readyState === WebSocket.OPEN) {
-      connection.send(JSON.stringify(o));
-    } else {
-      queue.push(o);
-    }
-  };
-
-  result.mount = function() {
-    connection = new WebSocket('ws://' + location.host + '/archae/engineWs');
+const client = () => ({
+  mount() {
+    const connection = new WebSocket('ws://' + location.host + '/archae/engineWs');
     connection.onopen = () => {
       if (queue.length > 0) {
         for (let i = 0; i < queue.length; i++) {
@@ -42,18 +23,33 @@ const client = () => {
       const {event, data} = m;
       _emit.call(result, event, data);
     };
+    let queue = [];
+
+    const result = new EventEmitter();
+
+    const _emit = result.emit;
+    result.emit = function(event, data) {
+      const o = {
+        event,
+        data,
+      };
+
+      if (connection.readyState === WebSocket.OPEN) {
+        connection.send(JSON.stringify(o));
+      } else {
+        queue.push(o);
+      }
+    };
 
     this._cleanup = () => {
       connection.close();
-      connection = null;
     };
 
     return result;
-  };
-  result.unmount = function() {
+  },
+  unmount() {
     this._cleanup();
-  };
-  return result;
-};
+  },
+});
 
 module.exports = client;
