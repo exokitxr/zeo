784dd899138a6381ad5ece76331168c9024f58d7
Adrian Biedrzycki <a@modules.io>
2016-11-20 01:21:51 -0800

    Exclude node_modules from plugin webpack builds

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const config = {
       {
         test: /\.js$/,
         // include: path.join(__dirname, 'plugins'),
-        // exclude: /(node_modules|bower_components)/,
+        exclude: /(node_modules|bower_components)/,
         loader: 'babel', // 'babel-loader' is also a valid name to reference
         query: {
           presets: ['es2015']
