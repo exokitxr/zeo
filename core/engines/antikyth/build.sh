d5522d4c717b2e7efc95476d57e461abdd9c2b24
Adrian Biedrzycki <a@modules.io>
2016-11-22 05:46:36 -0800

    Antikyth module build fixups

--- core/engines/antikyth/build.sh
+++ core/engines/antikyth/build.sh
@@ -4,5 +4,7 @@ if [ ! -f build/Release/physics.node ]; then
   pushd bullet3;
   ./build.sh;
   popd;
-  node-gyp rebuild --BULLET_PHYSICS_ROOT="$(pwd)"/bullet3;
+  cp binding-src.gyp binding.gyp; # HACK: this is so the module is not picked up as buildable until we explicitly build it here
+  ./node_modules/node-gyp/bin/node-gyp.js rebuild --BULLET_PHYSICS_ROOT="$(pwd)"/bullet3;
+  rm binding.gyp;
 fi;
