fd978765dfce3c8b12c73f7985a50d90daceced4
Adrian Biedrzycki <a@modules.io>
2016-11-23 10:54:46 -0800

    More multiplayer plugin work

--- extra/plugins/zeo/multiplayer/client.js
+++ extra/plugins/zeo/multiplayer/client.js
@@ -1,3 +1,8 @@
+const asyncJsonParse = require('async-json-parse');
+
+const hmdModelPath = '/archae/models/hmd/hmd.json';
+const controllerModelPath = '/archae/models/controller/controller.json';
+
 class Multiplayer {
   constructor(archae) {
     this._archae = archae;
@@ -21,50 +26,113 @@ class Multiplayer {
         const world = zeo.getCurrentWorld();
         const {player} = world;
 
-        const remotePlayerMeshes = new Map();
-
-        const _makeRemotePlayerMesh = status => {
-          // XXX
-        };
-        const _updateRemotePlayerMesh = (remotePlayerMesh, status) => {
-          // XXX
-        };
-
-        const playerStatuses = player.getPlayerStatuses();
-        playerStatuses.forEach((status, id) => {
-          const remotePlayerMesh = _makeRemotePlayerMesh(status);
-          scene.add(remotePlayerMesh);
-          remotePlayerMeshes.set(id, remotePlayerMesh);
+        const _requestMesh = modelPath => new Promise((accept, reject) => {
+          fetch(modelPath)
+            .then(res =>
+              res.text()
+                .then(s => asyncJsonParse(s))
+                .then(modelJson => new Promise((accept, reject) => {
+                  const loader = new THREE.ObjectLoader();
+                  loader.parse(modelJson, accept);
+                }))
+            )
+            .then(accept)
+            .catch(reject);
         });
+        const _requestHmdMesh = () => _requestMesh(hmdModelPath)
+          .then(mesh => {
+            mesh.scale.set(0.045, 0.045, 0.045);
+            mesh.rotation.order = camera.rotation.order;
+            mesh.rotation.y = Math.PI;
 
-        const playerStatusUpdate = update => {
-          const {id, status} = update;
-          const remotePlayerMesh = remotePlayerMeshes.get(id);
-          _updateRemotePlayerMesh(remotePlayerMesh, status);
-        };
-        const playerEnter = update => {
-          const {id, status} = update;
-          const remotePlayerMesh = _makeRemotePlayerMesh(status);
-          scene.add(remotePlayerMesh);
-          remotePlayerMeshes.set(id, remotePlayerMesh);
-        };
-        const playerLeave = update => {
-          const {id} = update;
-          const remotePlayerMesh = remotePlayerMeshes.get(id);
-          scene.remove(remotePlayerMesh);
-          remotePlayerMeshes.delete(id);
-        };
-        player.on('playerStatusUpdate', playerStatusUpdate);
-        player.on('playerEnter', playerEnter);
-        player.on('playerLeave', playerLeave);
-
-        // XXX push status updates with player.updateStatus when controller/camera position changes
-
-        this._cleanup = () => {
-          remotePlayerMeshes.forEach(remotePlayerMesh => {
-            scene.remove(remotePlayerMesh);
+            return mesh;
           });
-        };
+        const _requestControllerMesh = () => _requestMesh(controllerModelPath);
+
+        return Promise.all([
+          requestHmdMesh(),
+          requestControllerMesh(),
+        ]).then(([
+          hmdMesh,
+          controllerMesh,
+        ]) => {
+          const remotePlayerMeshes = new Map();
+
+          const _makeRemotePlayerMesh = status => {
+            const object = new THREE.Object3D();
+
+            const hmd = hmdMesh.clone();
+            object.add(hmd);
+            object.hmd = hmd;
+
+            const controllers = (() => {
+              const result = [controllerMesh.clone(), controllerMesh.clone()];
+              result.left = result[0];
+              result.right = result[1];
+              return result;
+            })();
+            controllers.forEach(controller => {
+              object.add(controller);
+            });
+            object.controllers = controller;
+
+            _updateRemotePlayerMesh(object, status);
+
+            return object;
+          };
+          const _updateRemotePlayerMesh = (remotePlayerMesh, status) => {
+            const {hmd, controllers} = remotePlayerMesh;
+            const {left: leftController, right: rightController} = controllers;
+
+            const {hmd: hmdStatus, controllers: controllersStatus} = status;
+            const {left: leftControllerStatus, right: rightControllerStatus} = controllersStatus;
+
+            hmd.position.fromArray(hmdStatus.position);
+            hmd.rotation.fromArray(hmdStatus.rotation);
+
+            leftController.position.fromArray(leftControllerStatus.position);
+            leftController.rotation.fromArray(leftControllerStatus.rotation);
+
+            rightController.position.fromArray(rightControllerStatus.position);
+            rightController.rotation.fromArray(rightControllerStatus.rotation);
+          };
+
+          const playerStatuses = player.getPlayerStatuses();
+          playerStatuses.forEach((status, id) => {
+            const remotePlayerMesh = _makeRemotePlayerMesh(status);
+            scene.add(remotePlayerMesh);
+            remotePlayerMeshes.set(id, remotePlayerMesh);
+          });
+
+          const playerStatusUpdate = update => {
+            const {id, status} = update;
+            const remotePlayerMesh = remotePlayerMeshes.get(id);
+            _updateRemotePlayerMesh(remotePlayerMesh, status);
+          };
+          const playerEnter = update => {
+            const {id, status} = update;
+            const remotePlayerMesh = _makeRemotePlayerMesh(status);
+            scene.add(remotePlayerMesh);
+            remotePlayerMeshes.set(id, remotePlayerMesh);
+          };
+          const playerLeave = update => {
+            const {id} = update;
+            const remotePlayerMesh = remotePlayerMeshes.get(id);
+            scene.remove(remotePlayerMesh);
+            remotePlayerMeshes.delete(id);
+          };
+          player.on('playerStatusUpdate', playerStatusUpdate);
+          player.on('playerEnter', playerEnter);
+          player.on('playerLeave', playerLeave);
+
+          // XXX push status updates with player.updateStatus when controller/camera position changes
+
+          this._cleanup = () => {
+            remotePlayerMeshes.forEach(remotePlayerMesh => {
+              scene.remove(remotePlayerMesh);
+            });
+          };
+        });
       }
     });
   }
---
+++ extra/plugins/zeo/multiplayer/server.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+class Multiplayer {
+  mount() {
+    const {express, app} = archae.getCore();
+
+    const hmdModelStatic = express.static(path.join(__dirname, 'models', 'hmd'));
+    const controllerModelStatic = express.static(path.join(__dirname, 'models', 'controller'));
+    function serveHmdModel(req, res, next) {
+      hmdModelStatic(req, res, next);
+    }
+    function serveControllerModel(req, res, next) {
+      controllerModelStatic(req, res, next);
+    }
+    app.use('/archae/models/hmd', serveHmdModel);
+    app.use('/archae/models/controller', serveControllerModel);
+
+    this._cleanup = () => {
+      function removeMiddlewares(route, i, routes) {
+        if (route.handle.name === 'serveHmdModel' || route.handle.name === 'serveControllerModel') {
+          routes.splice(i, 1);
+        }
+        if (route.route) {
+          route.route.stack.forEach(removeMiddlewares);
+        }
+      }
+      app._router.stack.forEach(removeMiddlewares);
+    };
+  },
+  unmount() {
+    this._cleanup();
+  },
+}
+
+module.exports = Multiplayer;
