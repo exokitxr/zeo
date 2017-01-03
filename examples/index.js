f31cd9bd3ec4ee99f7a9a9cb7af4406437254f51
Adrian Biedrzycki <a@modules.io>
2017-01-03 06:11:24 -0800

    Add new index.html

--- examples/index.js
+++ examples/index.js
@@ -4,33 +4,20 @@ const express = require('express');
 const archae = require('..');
 
 const app = express();
+app.get('/vr', (req, res, next) => {
+  req.url = '/vr.html';
+
+  next('route');
+});
 app.use('/', express.static(__dirname));
 
 const a = archae({
   app,
 });
-
-a.requestEngines([
-  // '/core/engines/express',
-  // '/core/engines/react',
-  // '/core/engines/ws',
-  // '/core/engines/bus',
-  // '/core/engines/three',
-  // '/core/engines/nedb',
-  // '/core/engines/biolumi',
-  // '/core/engines/heartlink',
-])
-  .then(() => {
-    console.log('request engines ok');
-
-    a.server.listen(8000);
-    a.server.on('listening', () => {
-      console.log('listening');
-    });
-    a.server.on('error', err => {
-      console.warn(err);
-    });
-  })
-  .catch(err => {
-    console.warn('request engines fail', err);
-  });
+a.server.listen(8000);
+a.server.on('listening', () => {
+  console.log('listening');
+});
+a.server.on('error', err => {
+  console.warn(err);
+});;
