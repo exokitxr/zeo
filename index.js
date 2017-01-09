2e38cb210c546cec1a4083ea6005e0f56fa5c515
Adrian Biedrzycki <a@modules.io>
2017-01-09 03:09:36 -0800

    Clean up package dependencies

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 const path = require('path');
 const http = require('http');
-
-const express = require('express');
 const archae = require('archae');
 
-const app = express();
-app.get('/vr', (req, res, next) => {
+const a = archae({
+  dirname: __dirname,
+  hostname: 'zeo.sh',
+  port: 8000,
+  publicDirectory: 'public',
+  dataDirectory: 'data',
+});
+a.app.get('/vr', (req, res, next) => {
   req.url = '/vr.html';
 
   next('route');
 });
-app.use('/', express.static(path.join(__dirname, 'public')));
-
-const a = archae({
-  dirname: __dirname,
-  app,
-});
-a.server.listen(8000);
-a.server.on('listening', () => {
-  console.log('listening');
-});
-a.server.on('error', err => {
-  console.warn(err);
+a.listen(err => {
+  if (!err) {
+    console.log('listening');
+  } else {
+    console.warn(err);
+  }
 });
