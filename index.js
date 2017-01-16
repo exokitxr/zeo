33f1cd4df27ccfc1a04281a527169159192222f3
Adrian Biedrzycki <a@modules.io>
2017-01-15 22:23:45 -0800

    Add static site mode support with `npm start -- --site`

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const path = require('path');
-const http = require('http');
 const archae = require('archae');
+
+const args = process.argv.slice(2);
+const mode = args.includes('--site') ? 'site' : 'app';
 
 const a = archae({
   dirname: __dirname,
@@ -8,15 +9,23 @@ const a = archae({
   port: 8000,
   publicDirectory: 'public',
   dataDirectory: 'data',
+  staticSite: mode === 'site',
 });
-a.app.get('/vr', (req, res, next) => {
-  req.url = '/vr.html';
-
-  next('route');
-});
+if (mode === 'site') {
+  a.app.get('/', (req, res, next) => {
+    req.url = '/site.html';
+    a.app(req, res, next);
+  });
+} else if (mode === 'app') {
+  a.app.get('/', (req, res, next) => {
+    req.url = '/vr.html';
+    a.app(req, res, next);
+  });
+}
 a.listen(err => {
   if (!err) {
-    console.log('listening');
+    console.log('listening in ' + mode.toUpperCase() + ' mode');
+    console.log('https://zeo.sh:8000/');
   } else {
     console.warn(err);
   }
