04908d76ec960c666d5ba1c935f4ed9bf3eb1427
Adrian Biedrzycki <a@modules.io>
2017-01-20 04:12:31 -0800

    Bullet engine build debugging

--- core/engines/bullet/scripts/postinstall.sh
+++ core/engines/bullet/scripts/postinstall.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 if [ ! -f build/Release/physics.node ]; then
-  pushd bullet3;
-  ./build.sh;
-  popd;
-  ./node_modules/node-gyp/bin/node-gyp.js rebuild --BULLET_PHYSICS_ROOT="$(pwd)"/bullet3;
+  cp binding.js binding.gyp
+  ../node-gyp/bin/node-gyp.js rebuild --BULLET_PHYSICS_ROOT="$(pwd)"/bullet3;
+  rm ./binding.gyp
 fi;
--- core/engines/bullet/scripts/preinstall.sh
+++ core/engines/bullet/scripts/preinstall.sh
@@ -4,5 +4,4 @@ if [ ! -f build/Release/physics.node ]; then
   pushd bullet3;
   ./build.sh;
   popd;
-  ./node_modules/node-gyp/bin/node-gyp.js rebuild --BULLET_PHYSICS_ROOT="$(pwd)"/bullet3;
 fi;
