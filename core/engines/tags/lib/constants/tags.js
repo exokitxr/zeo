1c5c3939d2099a0e584405b63cb40c79e0a838d2
Adrian Biedrzycki <a@modules.io>
2017-02-01 04:41:06 -0800

    Clean up tags rendering

--- core/engines/tags/lib/constants/tags.js
+++ core/engines/tags/lib/constants/tags.js
@@ -2,6 +2,6 @@ export const WIDTH = 400;
 export const HEIGHT = 150;
 export const ASPECT_RATIO = WIDTH / HEIGHT;
 
-export const WORLD_WIDTH = 0.1;
+export const WORLD_WIDTH = 0.2;
 export const WORLD_HEIGHT = WORLD_WIDTH / ASPECT_RATIO;
 export const WORLD_DEPTH = 0.02;
--- core/engines/tags/lib/render/tags.js
+++ core/engines/tags/lib/render/tags.js
@@ -1,15 +1,5 @@
 const makeRenderer = ({creatureUtils}) => {
 
-/* const getTagSrc = ({displayName, description, version}) => `\
-  <div style="display: flex; width: 400px; height: 150px;">
-    <div style="width: 100px;"></div>
-    <div style="width: 300px; flex-grow: 1;">
-      <h1 style="margin: 0; font-size: 32px; line-height: 1.4;">${displayName}</h1>
-      <p style="margin: 0; font-size: 16px; line-height: 1.4;">${description}</p>
-    </div>
-  </div>
-`; */
-
 const getTagSrc = ({displayName, description, version}) => `\
   <div style="display: flex; width: 400px; height: 150px; text-decoration: none;">
     <div style="width: 100px; height: 100px; margin: 0 10px;"></div>
