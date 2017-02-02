b7df7c79740297fdeb1a0600afaa976db37fd272
Adrian Biedrzycki <a@modules.io>
2017-02-01 21:18:17 -0800

    Add paint plugin brush support

---
+++ plugins/paint/server.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+class Paint {
+  constructor(archae) {
+    this._archae = archae;
+  }
+
+  mount() {
+    const {_archae: archae} = this;
+    const {express, app} = archae.getCore();
+
+    const brushesStatic = express.static(path.join(__dirname, 'brushes'));
+    function serveBrushes(req, res, next) {
+      brushesStatic(req, res, next);
+    }
+    app.use('/archae/paint/brushes', serveBrushes);
+
+    this._cleanup = () => {
+      function removeMiddlewares(route, i, routes) {
+        if (route.handle.name === 'serveBrushes') {
+          routes.splice(i, 1);
+        }
+        if (route.route) {
+          route.route.stack.forEach(removeMiddlewares);
+        }
+      }
+      app._router.stack.forEach(removeMiddlewares);
+    };
+  }
+
+  unmount() {
+    this._cleanup();
+  }
+}
+
+module.exports = Paint;
