eeb25791b63c305d4f1db2155b245a0fca419bd4
Adrian Biedrzycki <a@modules.io>
2017-02-11 00:39:57 -0800

    Add tags and files expand icons

--- core/engines/adventure/lib/render/adventure.js
+++ core/engines/adventure/lib/render/adventure.js
@@ -1,6 +1,6 @@
 const timeago = require('time-ago')();
-const KARMA_ICON = require('../img/karma-white');
-const KARMA_ICON_SRC = 'data:image/svg+xml;base64,' + btoa(KARMA_ICON);
+const karmaIcon = require('../img/karma-white');
+const karmaIconSrc = 'data:image/svg+xml;base64,' + btoa(karmaIcon);
 
 const makeRenderer = ({creatureUtils}) => {
 
@@ -8,7 +8,7 @@ const getAdventureSrc = ({id, name, author, created}) => `\
   <div style="background-color: #FFF;">
     <div style="padding-left: 30px; background-color: #000; color: #FFF; font-size: 40px; line-height: 80px;">
       <div style="display: inline-flex; width: 300px; float: right; background: #4CAF50; color: #FFF; font-size: 30px; justify-content: center; align-items: center; box-sizing: border-box; box-sizing: border-box;">
-        <img src="${KARMA_ICON_SRC}" width=34 height=34 style="margin-right: 10px;"> 350
+        <img src="${karmaIconSrc}" width=34 height=34 style="margin-right: 10px;"> 350
       </div>
       <div>${name}</div>
     </div>
--- core/engines/tags/lib/render/tags.js
+++ core/engines/tags/lib/render/tags.js
@@ -1,15 +1,22 @@
+/* const arrowRightImg = require('../img/arrow-right');
+const arrowRightImgSrc = 'data:image/svg+xml;base64,' + btoa(arrowRightImg);
+const dotsImg = require('../img/dots');
+const dotsImgSrc = 'data:image/svg+xml;base64,' + btoa(dotsImg); */
+const barsImg = require('../img/bars');
+const barsImgSrc = 'data:image/svg+xml;base64,' + btoa(barsImg);
+
 const getTagSrc = ({displayName, description, version, instancing}) => `\
   <div style="display: flex; width: 400px; height: 150px; background-color: #F0F0F0; text-decoration: none; ${instancing ? 'filter: brightness(75%);' : ''}">
-    <div style="display: flex; position: absolute; top: -15px; right: -60px; width: 155px; padding-top: 30px; padding-bottom: 10px; background-color: #2196F3; color: #FFF; justify-content: center; align-items: center; box-sizing: border-box; transform: rotate(45deg);">Mod</div>
+    <div style="display: flex; position: absolute; top: -15px; right: -58px; width: 155px; padding-top: 30px; padding-bottom: 10px; background-color: #2196F3; color: #FFF; justify-content: center; align-items: center; box-sizing: border-box; transform: rotate(45deg);">Mod</div>
     <div style="width: 100px; height: 100px; margin: 0 10px;"></div>
-    <div style="width: 270px; margin-right: 10px;">
+    <div style="width: 190px; margin-right: 10px;">
       <div style="height: 100px;">
         <h1 style="margin: 0; margin-top: 10px; font-size: 28px; line-height: 1.4;">${displayName}</h1>
         <p style="margin: 0; font-size: 15px; line-height: 1.4;">${description}</p>
       </div>
-      <div style="display: flex; height: 30px; margin-left: -110px; margin-bottom: 20px; justify-content: flex-end; align-items: center;">
-        <div style="padding: 5px 20px; border: 1px solid; border-radius: 100px; box-sizing: border-box;">Properties</div>
-      </div>
+    </div>
+    <div style="display: flex; width: 80px; justify-content: center; align-items: center;">
+      <img src="${barsImgSrc}" width="50" height="50">
     </div>
   </div>
 `;
