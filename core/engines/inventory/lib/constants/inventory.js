6f4928f136a9c5ebb96bcb400e5601d7875145b6
Adrian Biedrzycki <a@modules.io>
2017-02-15 23:12:10 -0800

    Remove dead inventory engine files

--- core/engines/inventory/lib/constants/inventory.js
+++
@@ -1,6 +0,0 @@
-export const WIDTH = 1024;
-export const HEIGHT = WIDTH / 1.5;
-export const ASPECT_RATIO = WIDTH / HEIGHT;
-export const WORLD_WIDTH = 2;
-export const WORLD_HEIGHT = WORLD_WIDTH / ASPECT_RATIO;
-export const WORLD_DEPTH = 0.01;
