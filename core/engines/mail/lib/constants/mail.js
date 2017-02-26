ca42717e59b74ba9da80cd5d14bc82e1b786e810
Adrian Biedrzycki <a@modules.io>
2017-02-26 09:40:27 -0800

    Bugfix height constants being non-integers in derivation. Fixes #55.

--- core/engines/mail/lib/constants/mail.js
+++ core/engines/mail/lib/constants/mail.js
@@ -1,5 +1,5 @@
 export const WIDTH = 1024;
-export const HEIGHT = WIDTH / 1.5;
+export const HEIGHT = Math.round(WIDTH / 1.5);
 export const ASPECT_RATIO = WIDTH / HEIGHT;
 export const WORLD_WIDTH = 2;
 export const WORLD_HEIGHT = WORLD_WIDTH / ASPECT_RATIO;
