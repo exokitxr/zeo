cda30deecc7eded48f9d02891de58f0dbab28dba
Adrian Biedrzycki <a@modules.io>
2017-02-27 00:23:02 -0800

    Delete old mail mesh code in mail engine

--- core/engines/mail/client.js
+++ core/engines/mail/client.js
@@ -16,15 +16,6 @@ import menuUtils from './lib/utils/menu';
 
 const SIDES = ['left', 'right'];
 
-const DEFAULT_GRAB_RADIUS = 0.2;
-const DEFAULT_MATRIX = [
-  0, 0, 0,
-  0, 0, 0, 1,
-  1, 1, 1,
-];
-
-const mailFlagSymbol = Symbol();
-
 class Mail {
   constructor(archae) {
     this._archae = archae;
@@ -114,54 +105,6 @@ class Mail {
                 scene.add(boxMeshes.left);
                 scene.add(boxMeshes.right);
 
-                const mailHoverStates = {
-                  left: biolumi.makeMenuHoverState(),
-                  right: biolumi.makeMenuHoverState(),
-                };
-                const mailDotMeshes = {
-                  left: biolumi.makeMenuDotMesh(),
-                  right: biolumi.makeMenuDotMesh(),
-                };
-                scene.add(mailDotMeshes.left);
-                scene.add(mailDotMeshes.right);
-                const mailBoxMeshes = {
-                  left: biolumi.makeMenuBoxMesh(),
-                  right: biolumi.makeMenuBoxMesh(),
-                };
-                scene.add(mailBoxMeshes.left);
-                scene.add(mailBoxMeshes.right);
-
-                const _makeGrabState = () => ({
-                  grabber: null,
-                });
-                const grabStates = {
-                  left: _makeGrabState(),
-                  right: _makeGrabState(),
-                };
-
-                const _makeGrabBoxMesh = () => {
-                  const width = MAIL_WORLD_WIDTH;
-                  const height = MAIL_WORLD_HEIGHT;
-                  const depth = MAIL_WORLD_DEPTH;
-
-                  const geometry = new THREE.BoxBufferGeometry(width, height, depth);
-                  const material = wireframeMaterial;
-
-                  const mesh = new THREE.Mesh(geometry, material);
-                  mesh.position.y = 1.2;
-                  mesh.rotation.order = camera.rotation.order;
-                  mesh.rotation.y = Math.PI / 2;
-                  mesh.depthWrite = false;
-                  mesh.visible = false;
-                  return mesh;
-                };
-                const grabBoxMeshes = {
-                  left: _makeGrabBoxMesh(),
-                  right: _makeGrabBoxMesh(),
-                };
-                scene.add(grabBoxMeshes.left);
-                scene.add(grabBoxMeshes.right);
-
                 const mailState = {
                   page: 'threads',
                 };
@@ -220,61 +163,24 @@ class Mail {
                 rend.registerMenuMesh('mailMesh', menuMesh);
 
                 const _updatePages = menuUtils.debounce(next => {
-                  const pageSpecs = (() => {
-                    const result = [];
+                  const pages = menuUi.getPages();
 
-                    const menuPages = menuUi.getPages();
-                    for (let i = 0; i < menuPages.length; i++) {
-                      const menuPage = menuPages[i];
-                      result.push({
-                        page: menuPage,
-                      });
-                    }
-
-                    for (let i = 0; i < mailMeshes.length; i++) {
-                      const mailMesh = mailMeshes[i];
-                      const {ui, mail} = mailMeshes;
-
-                      if (ui) {
-                        const pages = ui.getPages();
-
-                        for (let j = 0; j < pages.length; j++) {
-                          const page = pages[j];
-                          const pageSpec = {
-                            page,
-                            mail,
-                          };
-                          result.push(pageSpec);
-                        }
-                      }
-                    }
-
-                    return result;
-                  })();
-
-                  if (pageSpecs.length > 0) {
-                    let pending = pageSpecs.length;
+                  if (pages.length > 0) {
+                    let pending = pages.length;
                     const pend = () => {
                       if (--pending === 0) {
                         next();
                       }
                     };
 
-                    for (let i = 0; i < pageSpecs.length; i++) {
-                      const pageSpec = pageSpecs[i];
-                      const {page} = pageSpec;
+                    for (let i = 0; i < pages.length; i++) {
+                      const page = pages[i];
                       const {type} = page;
 
                       if (type === 'main') {
                         page.update({
                           mail: mailState,
                           focus: focusState,
-                        }, pend);
-                      } else if (type === 'mail') {
-                        const {mail} = pageSpec;
-
-                        page.update({
-                          mail,
                         }, pend);
                       } else {
                         pend();
@@ -318,25 +224,6 @@ class Mail {
                   }
                 };
                 input.on('trigger', _trigger);
-                const _gripdown = e => {
-                  const {side} = e;
-
-                  const bestGrabbableMailMesh = hands.getBestGrabbable(side, mailMeshes, {radius: DEFAULT_GRAB_RADIUS});
-                  if (bestGrabbableMailMesh) {
-                    mailInstance.grabMail(side, bestGrabbableMailMesh);
-                  }
-                };
-                input.on('gripdown', _gripdown);
-                const _gripup = e => {
-                  const {side} = e;
-                  const grabState = grabStates[side];
-                  const {grabber} = grabState;
-
-                  if (grabber) {
-                    grabber.release();
-                  }
-                };
-                input.on('gripup', _gripup);
                 const _update = () => {
                   const _updateMenuTextures = () => {
                     const tab = rend.getTab();
@@ -353,48 +240,6 @@ class Mail {
                         uiTime,
                       });
                     }
-                  };
-                  const _updateMailTextures = () => {
-                    const uiTime = rend.getUiTime();
-
-                    for (let i = 0; i < mailMeshes.length; i++) {
-                      const mailMesh = mailMeshes[i];
-                      const {
-                        ui,
-                        planeMesh,
-                      } = mailMesh;
-
-                      if (ui && planeMesh) {
-                        const {menuMaterial} = planeMesh;
-
-                        biolumi.updateMenuMaterial({
-                          ui,
-                          menuMaterial,
-                          uiTime,
-                        });
-                      }
-                    }
-                  };
-                  const _updateGrabbers = () => {
-                    SIDES.forEach(side => {
-                      const grabState = grabStates[side];
-                      const grabBoxMesh = grabBoxMeshes[side];
-
-                      const bestGrabbableMailMesh = hands.getBestGrabbable(side, mailMeshes, {radius: DEFAULT_GRAB_RADIUS});
-                      if (bestGrabbableMailMesh) {
-                        const {position: mailMeshPosition, rotation: mailMeshRotation} = _decomposeObjectMatrixWorld(bestGrabbableMailMesh);
-                        grabBoxMesh.position.copy(mailMeshPosition);
-                        grabBoxMesh.quaternion.copy(mailMeshRotation);
-
-                        if (!grabBoxMesh.visible) {
-                          grabBoxMesh.visible = true;
-                        }
-                      } else {
-                        if (grabBoxMesh.visible) {
-                          grabBoxMesh.visible = false;
-                        }
-                      }
-                    });
                   };
                   const _updateMenuAnchors = () => {
                     const tab = rend.getTab();
@@ -433,226 +278,21 @@ class Mail {
                       });
                     }
                   };
-                  const _updateMailAnchors = () => {
-                    const {gamepads} = webvr.getStatus();
-
-                    SIDES.forEach(side => {
-                      const gamepad = gamepads[side];
-
-                      if (gamepad) {
-                        const {position: controllerPosition, rotation: controllerRotation} = gamepad;
-                        const mailHoverState = mailHoverStates[side];
-                        const mailDotMesh = mailDotMeshes[side];
-                        const mailBoxMesh = mailBoxMeshes[side];
-
-                        biolumi.updateAnchors({
-                          objects: mailMeshes.map(mailMesh => {
-                            const {ui, planeMesh} = mailMesh;
-
-                            if (ui && planeMesh) {
-                              const matrixObject = _decomposeObjectMatrixWorld(planeMesh);
-
-                              return {
-                                matrixObject: matrixObject,
-                                ui: ui,
-                                width: MAIL_WIDTH,
-                                height: MAIL_HEIGHT,
-                                worldWidth: MAIL_WORLD_WIDTH,
-                                worldHeight: MAIL_WORLD_HEIGHT,
-                                worldDepth: MAIL_WORLD_DEPTH,
-                              };
-                            } else {
-                              return null;
-                            }
-                          }).filter(object => object !== null),
-                          hoverState: mailHoverState,
-                          dotMesh: mailDotMesh,
-                          boxMesh: mailBoxMesh,
-                          controllerPosition,
-                          controllerRotation,
-                        });
-                      }
-                    });
-                  };
 
                   _updateMenuTextures();
-                  _updateMailTextures();
-                  _updateGrabbers();
                   _updateMenuAnchors();
-                  _updateMailAnchors();
                 };
                 rend.on('update', _update);
 
                 this._cleanup = () => {
-                  for (let i = 0; i < mailMeshes.length; i++) {
-                    const mailMesh = mailMeshes[i];
-                    mailMesh.parent.remove(mailMesh);
-                  }
-
                   SIDES.forEach(side => {
                     scene.remove(dotMeshes[side]);
                     scene.remove(boxMeshes[side]);
-                    scene.remove(mailDotMesh[side]);
-                    scene.remove(mailBoxMesh[side]);
-                    scene.remove(grabBoxMesh[side]);
                   });
 
                   input.removeListener('trigger', _trigger);
-                  input.removeListener('gripdown', _gripdown);
-                  input.removeListener('gripup', _gripup);
                   rend.removeListener('update', _update);
                 };
-
-                class Mail {
-                  constructor(id, name, author, created, matrix) {
-                    this.id = id;
-                    this.name = name;
-                    this.author = author;
-                    this.created = created;
-                    this.matrix = matrix;
-                  }
-                }
-
-                const mailMeshes = [];
-                class MailApi {
-                  makeMail(mailSpec) {
-                    const object = new THREE.Object3D();
-                    object[mailFlagSymbol] = true;
-
-                    const mail = new Mail(mailSpec.id, mailSpec.name, mailSpec.author, mailSpec.created, mailSpec.matrix);
-                    object.mail = mail;
-
-                    object.position.set(mail.matrix[0], mail.matrix[1], mail.matrix[2]);
-                    object.quaternion.set(mail.matrix[3], mail.matrix[4], mail.matrix[5], mail.matrix[6]);
-                    object.scale.set(mail.matrix[7], mail.matrix[8], mail.matrix[9]);
-
-                    object.ui = null;
-                    object.planeMesh = null;
-
-                    this._requestDecorateMail(object);
-
-                    mailMeshes.push(object);
-
-                    return object;
-                  }
-
-                  _requestDecorateMail(object) {
-                    return biolumi.requestUi({
-                      width: MAIL_WIDTH,
-                      height: MAIL_HEIGHT,
-                    })
-                      .then(ui => {
-                        const {mail} = object;
-
-                        ui.pushPage(({mail}) => ([
-                          {
-                            type: 'html',
-                            src: mailRenderer.getMailSrc(mail),
-                          },
-                          /* {
-                            type: 'image',
-                            img: creatureUtils.makeAnimatedCreature('mail:' + mail.name),
-                            x: 10,
-                            y: 0,
-                            w: 100,
-                            h: 100,
-                            frameTime: 300,
-                            pixelated: true,
-                          } */
-                        ]), {
-                          type: 'mail',
-                          state: {
-                            mail,
-                          },
-                          immediate: true,
-                        });
-                        object.ui = ui;
-
-                        _updatePages();
-
-                        const planeMesh = (() => {
-                          const width = MAIL_WORLD_WIDTH;
-                          const height = MAIL_WORLD_HEIGHT;
-                          const depth = MAIL_WORLD_DEPTH;
-
-                          const menuMaterial = biolumi.makeMenuMaterial();
-
-                          const geometry = new THREE.PlaneBufferGeometry(width, height);
-                          const materials = [solidMaterial, menuMaterial];
-
-                          const mesh = THREE.SceneUtils.createMultiMaterialObject(geometry, materials)
-                          // mesh.position.y = 1.5;
-                          mesh.receiveShadow = true;
-                          mesh.menuMaterial = menuMaterial;
-
-                          return mesh;
-                        })();
-                        object.add(planeMesh);
-                        object.planeMesh = planeMesh;
-
-                        const shadowMesh = (() => {
-                          const geometry = new THREE.BoxBufferGeometry(MAIL_WORLD_WIDTH, MAIL_WORLD_HEIGHT, 0.01);
-                          const material = transparentMaterial.clone();
-                          material.depthWrite = false;
-
-                          const mesh = new THREE.Mesh(geometry, material);
-                          mesh.castShadow = true;
-                          return mesh;
-                        })();
-                        object.add(shadowMesh);
-                      });
-                  };
-
-                  destroyMail(mailMesh) {
-                    const index = mailMeshes.indexOf(mailMesh);
-
-                    if (index !== -1) {
-                      mailMeshes.splice(index, 1);
-                    }
-                  }
-
-                  isMail(object) {
-                    return object[mailFlagSymbol] === true;
-                  }
-
-                  grabMail(side, mailMesh) {
-                    const menuMesh = rend.getMenuMesh();
-                    menuMesh.add(mailMesh);
-
-                    const {mail} = mailMesh;
-                    mail.matrix = DEFAULT_MATRIX;
-
-                    const grabber = hands.grab(side, mailMesh);
-                    grabber.on('update', ({position, rotation}) => {
-                      const menuMeshMatrixInverse = new THREE.Matrix4().getInverse(menuMesh.matrix);
-                      const menuMeshQuaternionInverse = menuMesh.quaternion.clone().inverse();
-
-                      const newRotation = menuMeshQuaternionInverse.clone()
-                        .multiply(rotation)
-                        .multiply(new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0, 1, 0), new THREE.Vector3(0, 0, -1)));
-                      const newPosition = position.clone().applyMatrix4(menuMeshMatrixInverse)
-                        .add(
-                          new THREE.Vector3(0, 0.02, 0).applyQuaternion(newRotation)
-                        );
-
-                      mailMesh.position.copy(newPosition);
-                      mailMesh.quaternion.copy(newRotation);
-                    });
-                    grabber.on('release', () => {
-                      const {position, quaternion, mail} = mailMesh;
-                      const newMatrixArray = position.toArray().concat(quaternion.toArray()).concat(new THREE.Vector3(1, 1, 1).toArray());
-                      mail.matrix = newMatrixArray;
-
-                      grabState.grabber = null;
-                    });
-
-                    const grabState = grabStates[side];
-                    grabState.grabber = grabber;
-                  }
-                };
-
-                const mailInstance = new MailApi();
-                return mailInstance;
               }
             });
         }
@@ -663,7 +303,5 @@ class Mail {
     this._cleanup();
   }
 }
-
-const _clone = o => JSON.parse(JSON.stringify(o));
 
 module.exports = Mail;
--- core/engines/mail/lib/render/mail.js
+++ core/engines/mail/lib/render/mail.js
@@ -198,49 +198,9 @@ const getThreadSidebarSrc = () => {
   `;
 };
 
-const getMailSrc = ({id, name, author, created}) => `\
-  <div style="min-height: ${MAIL_HEIGHT}px; background-color: #FFF;">
-    <div style="padding-left: 30px; background-color: #000; color: #FFF; font-size: 40px; line-height: 80px;">
-      <div style="display: inline-flex; width: 300px; float: right; background: #4CAF50; color: #FFF; font-size: 30px; justify-content: center; align-items: center; box-sizing: border-box; box-sizing: border-box;">
-        <img src="${karmaWhiteIconSrc}" width=34 height=34 style="margin-right: 10px;"> 350
-      </div>
-      <div>${name}</div>
-    </div>
-    <div style="padding: 5px 10px;">
-      <div style="display: inline-flex; padding: 7px 20px; background-color: #EEE; border-radius: 100px; margin-bottom: 20px; font-size: ${30 / 1.4}px; line-height: 1.4;">
-        <img src="${creatureUtils.makeStaticCreature('user:' + author)}" width="30" height="30" style="margin-right: 10px; image-rendering: pixelated;" />
-        <div>
-          <span style="font-weight: 400;">${author}</span>
-          <span>posted</span>
-          <span style="font-weight: 400;">${timeago.ago(created)}</span>
-        </div>
-      </div>
-    </div>
-    <div style="display: flex; margin-bottom: 10px; padding: 0 10px; justify-content: space-between;">
-      <a style="display: flex; padding: 7px 20px; border: 1px solid #333; border-radius: 100px; font-size: 24px; font-weight: 400; line-height: 1.4; text-decoration: none; justify-content: center; align-items: center;" onclick="mail:setPosition">Set position</a>
-      <div style="display: flex; border: 1px solid #333; border-radius: 100px; font-size: 24px; font-weight: 400; line-height: 1.4; justify-content: center; align-items: center;">
-         <a style="padding: 7px 20px; text-decoration: none;" onclick="mail:time:minus">-</a>
-         <div style="display: flex; padding: 7px 20px; border-width: 0 1px 0 1px; border-style: solid; border-color: #333; justify-content: center; align-items: center;">
-           0:00:00
-         </div style="padding: 7px 20px;">
-         <a style="padding: 7px 20px; text-decoration: none;" onclick="mail:time:plus">+</a>
-      </div>
-      <div style="display: flex; border: 1px solid #333; border-radius: 100px; font-size: 24px; font-weight: 400; line-height: 1.4; justify-content: center; align-items: center;">
-         <a style="padding: 7px 20px; text-decoration: none;" onclick="mail:karma:minus">-</a>
-         <div style="display: flex; padding: 7px 20px; border-width: 0 1px 0 1px; border-style: solid; border-color: #333; justify-content: center; align-items: center;">
-           <img src="${karmaBlackIconSrc}" width=20 height=20 style="margin-right: 10px; width: 34px; height: 34px;"> 0
-          </div>
-         <a style="padding: 7px 20px; text-decoration: none;" onclick="mail:karma:plus">+</a>
-      </div>
-      <a style="display: flex; padding: 7px 20px; border: 1px solid #333; border-radius: 100px; font-size: 24px; font-weight: 400; line-height: 1.4; text-decoration: none; justify-content: center; align-items: center;" onclick="mail:cancel">Cancel</a>
-    </div>
-  </div>
-`;
-
 return  {
   getMailPageSrc,
   getThreadsPageSrc,
-  getMailSrc,
 };
 
 };
