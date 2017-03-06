3ea814882e36b6a76d94d7d6d7012d9f1b9d16c0
Adrian Biedrzycki <a@modules.io>
2017-03-05 17:21:00 -0800

    Make example start script start hub and two servers

--- scripts/example.sh
+++ scripts/example.sh
@@ -3,7 +3,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 pushd "$DIR"/../;
-npm start -- site hub server &
-npm start -- server port=8001 dataDirectory=data2 installDirectory=installed2 serverHost=server1.zeovr.io serverType=insecure hubUrl=hub.zeovr.io:8000 &
+npm start -- hub &
+npm start -- server port=8001 dataDirectory=data1 installDirectory=installed1 serverHost=server1.zeovr.io hubUrl=hub.zeovr.io:8000 &
+npm start -- server port=8002 dataDirectory=data2 installDirectory=installed2 serverHost=server2.zeovr.io hubUrl=hub.zeovr.io:8000 &
 sleep infinity;
 popd;
