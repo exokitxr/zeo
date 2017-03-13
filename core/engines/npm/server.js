31e2adaec6f1b30584b98fd314cbfaa8e1647a3f
Adrian Biedrzycki <a@modules.io>
2017-03-13 01:52:37 -0700

    Update engines and plugins to use zeo-mod keyword

--- core/engines/npm/server.js
+++ core/engines/npm/server.js
@@ -106,7 +106,7 @@ class Npm {
 
       https.get({
         hostname: 'api.npms.io',
-        path: '/v2/search?q=' + encodeURIComponent(q) + '+keywords:zeo-mod',
+        path: '/v2/search?q=' + encodeURIComponent(q) + '+keywords:zeo-module',
       }, proxyRes => {
         if (proxyRes.statusCode >= 200 && proxyRes.statusCode < 300) {
           _getJson(proxyRes, (err, j) => {
