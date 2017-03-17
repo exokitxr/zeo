d7d0b69382cd0be6754f0eba848175eee7ca1647
Adrian Biedrzycki <a@modules.io>
2017-03-17 14:33:06 -0700

    Tags engine render cleanup

--- core/engines/tags/lib/render/tags.js
+++ core/engines/tags/lib/render/tags.js
@@ -194,20 +194,20 @@ const makeRenderer = ({creatureUtils}) => {
     switch (type) {
       case 'matrix': {
         return `\
-  <div style="display: flex; width: ${WIDTH}px; justify-content: flex-end;">
-    <a style="display: flex; width: 80px; justify-content: center; align-items: center;" onclick="attribute:${id}:${name}:position" onmousedown="attribute:${name}:position">
-      <img src="${targetImgSrc}" width="50" height="50" style="mwidth: 80px; height: 80px; margin: 10px; image-rendering: pixelated;" />
-    </a>
-  </div>
-  `;
+          <div style="display: flex; width: ${WIDTH}px; justify-content: flex-end;">
+            <a style="display: flex; width: 80px; justify-content: center; align-items: center;" onclick="attribute:${id}:${name}:position" onmousedown="attribute:${name}:position">
+              <img src="${targetImgSrc}" width="50" height="50" style="mwidth: 80px; height: 80px; margin: 10px; image-rendering: pixelated;" />
+            </a>
+          </div>
+        `;
       }
       case 'text': {
         return `\
-  <a style="position: relative; width: ${width}px; height: 40px; border: 2px solid #333; text-decoration: none; overflow: hidden; box-sizing: border-box;" onclick="attribute:${id}:${name}:focus" onmousedown="attribute:${id}:${name}:focus">
-    ${focus ? `<div style="position: absolute; width: 2px; top: 0; bottom: 10px; left: ${inputValue}px; background-color: #333;"></div>` : ''}
-    <div>${focusValue}</div>
-  </a>
-  `;
+          <a style="position: relative; width: ${width}px; height: 40px; border: 2px solid #333; text-decoration: none; overflow: hidden; box-sizing: border-box;" onclick="attribute:${id}:${name}:focus" onmousedown="attribute:${id}:${name}:focus">
+            ${focus ? `<div style="position: absolute; width: 2px; top: 0; bottom: 10px; left: ${inputValue}px; background-color: #333;"></div>` : ''}
+            <div>${focusValue}</div>
+          </a>
+        `;
       }
       case 'number': {
         if (min === undefined) {
@@ -221,16 +221,16 @@ const makeRenderer = ({creatureUtils}) => {
         const string = focusValue !== null ? String(focusValue) : inputText;
 
         return `\
-  <a style="position: relative; width: ${width - (100 + 20)}px; height: 40px; margin-right: 20px;" onclick="attribute:${id}:${name}:tweak" onmousedown="attribute:${id}:${name}:tweak">
-    <div style="position: absolute; top: 19px; left: 0; right: 0; height: 2px; background-color: #CCC;">
-      <div style="position: absolute; top: -14px; bottom: -14px; left: ${factor * 100}%; margin-left: -1px; width: 2px; background-color: #F00;"></div>
-    </div>
-  </a>
-  <a style="position: relative; width: 100px; height: 40px; background-color: #EEE; border-radius: 5px; text-decoration: none; overflow: hidden;" onclick="attribute:${id}:${name}:focus" onmousedown="attribute:${id}:${name}:focus">
-    ${focus ? `<div style="position: absolute; width: 2px; top: 0; bottom: 10px; left: ${inputValue}px; background-color: #333;"></div>` : ''}
-    <div>${string}</div>
-  </a>
-  `;
+          <a style="position: relative; width: ${width - (100 + 20)}px; height: 40px; margin-right: 20px;" onclick="attribute:${id}:${name}:tweak" onmousedown="attribute:${id}:${name}:tweak">
+            <div style="position: absolute; top: 19px; left: 0; right: 0; height: 2px; background-color: #CCC;">
+              <div style="position: absolute; top: -14px; bottom: -14px; left: ${factor * 100}%; margin-left: -1px; width: 2px; background-color: #F00;"></div>
+            </div>
+          </a>
+          <a style="position: relative; width: 100px; height: 40px; background-color: #EEE; border-radius: 5px; text-decoration: none; overflow: hidden;" onclick="attribute:${id}:${name}:focus" onmousedown="attribute:${id}:${name}:focus">
+            ${focus ? `<div style="position: absolute; width: 2px; top: 0; bottom: 10px; left: ${inputValue}px; background-color: #333;"></div>` : ''}
+            <div>${string}</div>
+          </a>
+        `;
       }
       case 'select': {
         if (options === undefined) {
@@ -239,36 +239,36 @@ const makeRenderer = ({creatureUtils}) => {
 
         if (!focus) {
           return `\
-  <a style="display: flex; width: ${width}px; height: 40px; border: 2px solid #333; text-decoration: none; align-items: center; box-sizing: border-box;" onclick="attribute:${id}:${name}:focus" onmousedown="attribute:${id}:${name}:focus">
-    <div style="width: ${400 - 30}px; text-overflow: ellipsis; overflow: hidden;">${focusValue}</div>
-    <div style="display: flex; width: 30px; font-size: 16px; justify-content: center;">${unescape(encodeURIComponent('▼'))}</div>
-  </a>
-  `;
+            <a style="display: flex; width: ${width}px; height: 40px; border: 2px solid #333; text-decoration: none; align-items: center; box-sizing: border-box;" onclick="attribute:${id}:${name}:focus" onmousedown="attribute:${id}:${name}:focus">
+              <div style="width: ${400 - 30}px; text-overflow: ellipsis; overflow: hidden;">${focusValue}</div>
+              <div style="display: flex; width: 30px; font-size: 16px; justify-content: center;">${unescape(encodeURIComponent('▼'))}</div>
+            </a>
+          `;
         } else {
           return `\
-  <div style="position: relative; width: ${width}px; height: 40px; z-index: 1;">
-    <div style="display: flex; flex-direction: column; background-color: #FFF;">
-      ${options.map((option, i, a) => {
-        const style = (() => {
-          let result = '';
-          if (i !== 0) {
-            result += 'padding-top: 2px; border-top: 0;';
-          }
-          if (i !== (a.length - 1)) {
-            result += 'padding-bottom: 2px; border-bottom: 0;';
-          }
-          if (option === focusValue) {
-            result += 'background-color: #EEE;';
-          }
-          return result;
-        })();
-        return `<a style="display: flex; width: ${width}px; height: 40px; border: 2px solid #333; ${style}; text-decoration: none; align-items: center; text-overflow: ellipsis; overflow: hidden; box-sizing: border-box;" onclick="attribute:${id}:${name}:set:${option}" onmousedown="attribute:${id}:${name}:set:${option}">
-          ${option}
-        </a>`;
-      }).join('\n')}
-    </div>
-  </div>
-  `;
+            <div style="position: relative; width: ${width}px; height: 40px; z-index: 1;">
+              <div style="display: flex; flex-direction: column; background-color: #FFF;">
+                ${options.map((option, i, a) => {
+                  const style = (() => {
+                    let result = '';
+                    if (i !== 0) {
+                      result += 'padding-top: 2px; border-top: 0;';
+                    }
+                    if (i !== (a.length - 1)) {
+                      result += 'padding-bottom: 2px; border-bottom: 0;';
+                    }
+                    if (option === focusValue) {
+                      result += 'background-color: #EEE;';
+                    }
+                    return result;
+                  })();
+                  return `<a style="display: flex; width: ${width}px; height: 40px; border: 2px solid #333; ${style}; text-decoration: none; align-items: center; text-overflow: ellipsis; overflow: hidden; box-sizing: border-box;" onclick="attribute:${id}:${name}:set:${option}" onmousedown="attribute:${id}:${name}:set:${option}">
+                    ${option}
+                  </a>`;
+                }).join('\n')}
+              </div>
+            </div>
+          `;
         }
       }
       case 'color': {
@@ -276,44 +276,44 @@ const makeRenderer = ({creatureUtils}) => {
         const string = focusValue !== null ? focusValue : inputText;
 
         return `\
-  <div style="display: flex; width: ${width}px; height: 40px; align-items: center;">
-    <div style="width: 40px; height: 40px; margin-right: 4px; background-color: ${color};"></div>
-    <a style="position: relative; width: ${400 - (40 + 4)}px; height: 40px; border: 2px solid #333; text-decoration: none; overflow: hidden; box-sizing: border-box;" onclick="attribute:${id}:${name}:focus" onmousedown="attribute:${id}:${name}:focus">
-      ${focus ? `<div style="position: absolute; width: 2px; top: 0; bottom: 10px; left: ${inputValue}px; background-color: #333;"></div>` : ''}
-      <div>${string}</div>
-    </a>
-  </div>
-  `;
+          <div style="display: flex; width: ${width}px; height: 40px; align-items: center;">
+            <div style="width: 40px; height: 40px; margin-right: 4px; background-color: ${color};"></div>
+            <a style="position: relative; width: ${400 - (40 + 4)}px; height: 40px; border: 2px solid #333; text-decoration: none; overflow: hidden; box-sizing: border-box;" onclick="attribute:${id}:${name}:focus" onmousedown="attribute:${id}:${name}:focus">
+              ${focus ? `<div style="position: absolute; width: 2px; top: 0; bottom: 10px; left: ${inputValue}px; background-color: #333;"></div>` : ''}
+              <div>${string}</div>
+            </a>
+          </div>
+        `;
       }
       case 'checkbox': {
         return `\
-  <div style="display: flex; width: ${width}px; height: 40px; justify-content: flex-end; align-items: center;">
-    ${focusValue ?
-      `<a style="display: flex; width: 40px; height: 40px; justify-content: center; align-items: center;" onclick="attribute:${id}:${name}:toggle" onmousedown="attribute:${id}:${name}:toggle">
-        <div style="display: flex; width: ${(20 * 2) - (3 * 2)}px; height: 20px; padding: 1px; border: 3px solid #333; justify-content: flex-end; align-items: center; box-sizing: border-box;">
-          <div style="width: ${20 - ((3 * 2) + (1 * 2))}px; height: ${20 - ((3 * 2) + (1 * 2))}px; background-color: #333;"></div>
-        </div>
-      </a>`
-    :
-      `<a style="display: flex; width: 40px; height: 40px; justify-content: center; align-items: center;" onclick="attribute:${id}:${name}:toggle" onmousedown="attribute:${id}:${name}:toggle">
-        <div style="display: flex; width: ${(20 * 2) - (3 * 2)}px; height: 20px; padding: 1px; border: 3px solid #CCC; justify-content: flex-start; align-items: center; box-sizing: border-box;">
-          <div style="width: ${20 - ((3 * 2) + (1 * 2))}px; height: ${20 - ((3 * 2) + (1 * 2))}px; background-color: #CCC;"></div>
-        </div>
-      </a>`
-    }
-  </div>
-  `;
+          <div style="display: flex; width: ${width}px; height: 40px; justify-content: flex-end; align-items: center;">
+            ${focusValue ?
+              `<a style="display: flex; width: 40px; height: 40px; justify-content: center; align-items: center;" onclick="attribute:${id}:${name}:toggle" onmousedown="attribute:${id}:${name}:toggle">
+                <div style="display: flex; width: ${(20 * 2) - (3 * 2)}px; height: 20px; padding: 1px; border: 3px solid #333; justify-content: flex-end; align-items: center; box-sizing: border-box;">
+                  <div style="width: ${20 - ((3 * 2) + (1 * 2))}px; height: ${20 - ((3 * 2) + (1 * 2))}px; background-color: #333;"></div>
+                </div>
+              </a>`
+            :
+              `<a style="display: flex; width: 40px; height: 40px; justify-content: center; align-items: center;" onclick="attribute:${id}:${name}:toggle" onmousedown="attribute:${id}:${name}:toggle">
+                <div style="display: flex; width: ${(20 * 2) - (3 * 2)}px; height: 20px; padding: 1px; border: 3px solid #CCC; justify-content: flex-start; align-items: center; box-sizing: border-box;">
+                  <div style="width: ${20 - ((3 * 2) + (1 * 2))}px; height: ${20 - ((3 * 2) + (1 * 2))}px; background-color: #CCC;"></div>
+                </div>
+              </a>`
+            }
+          </div>
+        `;
       }
       case 'file': {
         return `\
-  <div style="display: flex; width: ${width}px; height: 40px;">
-    <a style="display: flex; position: relative; width: ${width - 100}px; height: 40px; margin-right: 20px; border: 2px solid #333; align-items: center; text-decoration: none; box-sizing: border-box;" onclick="attribute:${id}:${name}:focus" onmousedown="attribute:${id}:${name}:focus">
-      ${focus ? `<div style="position: absolute; width: 2px; top: 0; bottom: 10px; left: ${inputValue}px; background-color: #333;"></div>` : ''}
-      <div style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">${focusValue}</div>
-    </a>
-    <a style="display: flex; width: 100px; border: 2px solid #d9534f; border-radius: 5px; color: #d9534f; text-decoration: none; justify-content: center; align-items: center; box-sizing: border-box;" onclick="attribute:${id}:${name}:choose" onmousedown="attribute:${id}:${name}:choose">Choose</a>
-  </div>
-  `;
+          <div style="display: flex; width: ${width}px; height: 40px;">
+            <a style="display: flex; position: relative; width: ${width - 100}px; height: 40px; margin-right: 20px; border: 2px solid #333; align-items: center; text-decoration: none; box-sizing: border-box;" onclick="attribute:${id}:${name}:focus" onmousedown="attribute:${id}:${name}:focus">
+              ${focus ? `<div style="position: absolute; width: 2px; top: 0; bottom: 10px; left: ${inputValue}px; background-color: #333;"></div>` : ''}
+              <div style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">${focusValue}</div>
+            </a>
+            <a style="display: flex; width: 100px; border: 2px solid #d9534f; border-radius: 5px; color: #d9534f; text-decoration: none; justify-content: center; align-items: center; box-sizing: border-box;" onclick="attribute:${id}:${name}:choose" onmousedown="attribute:${id}:${name}:choose">Choose</a>
+          </div>
+        `;
       }
       default: {
         return '';
