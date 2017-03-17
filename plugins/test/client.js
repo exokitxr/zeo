80acb6d28fd003491fef68673522820ab3c25bfc
Adrian Biedrzycki <a@modules.io>
2017-03-17 14:30:48 -0700

    Add test plugin

---
+++ plugins/test/client.js
@@ -0,0 +1,69 @@
+class Test {
+  mount() {
+    const {elements} = zeo;
+
+    console.log('mount');
+
+    const testComponent = {
+      attributes: {
+        position: {
+          type: 'matrix',
+          value: [
+            0, 0, 0,
+            0, 0, 0, 1,
+            1, 1, 1
+          ]
+        },
+        text: {
+          type: 'text',
+          value: 'Some text',
+        },
+        number: {
+          type: 'number',
+          value: 1,
+          min: 0,
+          max: 10,
+          step: 1,
+        },
+        select: {
+          type: 'option',
+          value: 'Option A',
+          options: [
+            'Option A',
+            'Option B',
+          ],
+        },
+        color: {
+          type: 'color',
+          value: '#E91E63',
+        },
+        checkbox: {
+          type: 'checkbox',
+          value: false,
+        },
+      },
+      entityAddedCallback(entityElement, attribute, value) {
+        console.log('entityAddedCallback', {entityElement, attribute, value});
+      },
+      entityRemovedCallback(entityElement) {
+        console.log('entityRemovedCallback', {entityElement});
+      },
+      entityAttributeValueChangedCallback(entityElement, name, oldValue, newValue) {
+        console.log('entityAttributeValueChangedCallback', {entityElement, name, oldValue, newValue});
+      },
+    };
+    elements.registerComponent(this, testComponent);
+
+    this._cleanup = () => {
+      elements.unregisterComponent(this, testComponent);
+    };
+  }
+
+  unmount() {
+    console.log('unmount');
+
+    this._cleanup();
+  }
+}
+
+module.exports = Test;
