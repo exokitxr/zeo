9b4d09e3dbb61b217326943e90e4b2dcd1abf7bd
Adrian Biedrzycki <a@modules.io>
2017-03-21 00:07:47 -0700

    Frontend items args bugfix

--- core/engines/tags/client.js
+++ core/engines/tags/client.js
@@ -2273,6 +2273,7 @@ class Tags {
                 itemSpec.version,
                 itemSpec.tagName, // XXX get rid of these
                 itemSpec.attributes,
+                itemSpec.data,
                 itemSpec.mimeType,
                 itemSpec.matrix,
                 itemSpec.metadata
