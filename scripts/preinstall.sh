64de7f97977f7ed766dc82b374ed5e14ad4e5d6f
Adrian Biedrzycki <a@modules.io>
2017-03-26 10:10:29 -0700

    Remove dead preinstall script for building bullet engine

--- scripts/preinstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# prebuild bullet engine as an optimization
-pushd "$DIR"/../core/engines/bullet/ >/dev/null;
-npm install;
-popd >/dev/null;
