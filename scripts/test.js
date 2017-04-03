6c4d5c7370bcb2d121842ae6bc3c98606506b1b2
Adrian Biedrzycki <a@modules.io>
2017-04-03 13:10:56 -0700

    Use isolated test crypto directory for test script

--- scripts/test.js
+++ scripts/test.js
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 pushd "$DIR"/../;
-npm start -- site hub port=8000 hubUrl=test.zeovr.io:8000 &
+npm start -- site hub port=8000 hubUrl=test.zeovr.io:8000 cryptoDirectory=crypto-test &
 npm start -- server port=8001 dataDirectory=data/hub/servers/server1/data cryptoDirectory=data/hub/servers/server1/crypto installDirectory=data/hub/servers/server1/installed serverHost=server1.test.zeovr.io worldname='Server One' hubUrl=test.zeovr.io:8000 &
 npm start -- server port=8002 dataDirectory=data/hub/servers/server2/data cryptoDirectory=data/hub/servers/server2/crypto installDirectory=data/hub/servers/server2/installed serverHost=server2.test.zeovr.io worldname='Server Two' hubUrl=test.zeovr.io:8000 &
 sleep infinity;
