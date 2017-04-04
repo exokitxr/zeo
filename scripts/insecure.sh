b9f59501e186924ce1669f1ae0d5ae2d73701d46
Adrian Biedrzycki <a@modules.io>
2017-04-04 08:27:05 -0700

    Remove old insecure start script

--- scripts/insecure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-pushd "$DIR"/../ >/dev/null;
-npm start -- server serverHost=insecure.zeovr.io hubUrl=none worldname=zeo &
-sleep infinity;
-popd >/dev/null;
