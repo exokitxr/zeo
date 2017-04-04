9fda938fcc0e92486e6fcf7afde3662548f4a958
Adrian Biedrzycki <a@modules.io>
2017-04-04 08:32:17 -0700

    Add my start script

---
+++ scripts/my.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pushd "$DIR"/../;
+export NODE_TLS_REJECT_UNAUTHORIZED=0
+npm start -- home my port=8888 homeUrl=my.zeovr.io:8888 hubUrl=hub.zeovr.io:8000 cryptoDirectory=crypto &
+sleep infinity;
+popd;
