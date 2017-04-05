ad42678d14bb8b0fe89d407f28481644204c7798
Adrian Biedrzycki <a@modules.io>
2017-04-04 23:50:19 -0700

    Remove postinstall script

--- scripts/postinstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# preinstall core engines as an optimization
-pushd "$DIR"/../ >/dev/null;
-npm start --unsafe-perm -- install;
-popd >/dev/null;
