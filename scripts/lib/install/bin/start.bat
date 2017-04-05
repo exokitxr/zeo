62973f3ee75b53bfb404939b51c7608c37b0b03f
Adrian Biedrzycki <a@modules.io>
2017-04-04 23:55:33 -0700

    Remove all old windows install scripts

--- scripts/lib/install/bin/start.bat
+++
@@ -1,1 +0,0 @@
-bash -c 'PATH="$(pwd)/pkg/node/bin:$PATH" ^&^& ./scripts/my.sh'
--- scripts/lib/install/symlink/unpack-symlinks.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-while IFS='' read -r src && read -r dst; do
-  ln -s "$src" "$dst";
-done </dev/stdin;
