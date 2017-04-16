a2b32b73499f06ff84c8c8601ce2075f57dc82c3
Adrian Biedrzycki <a@modules.io>
2017-04-16 12:35:08 -0700

    Clean up player label mesh rendering

--- core/engines/assets/lib/constants/menu.js
+++ core/engines/assets/lib/constants/menu.js
@@ -1,5 +1,5 @@
 export const WIDTH = 1024;
-export const HEIGHT = Math.round(WIDTH / 5);
+export const HEIGHT = Math.round(WIDTH / 8);
 export const ASPECT_RATIO = WIDTH / HEIGHT;
 export const WORLD_WIDTH = 0.75;
 export const WORLD_HEIGHT = WORLD_WIDTH / ASPECT_RATIO;
--- core/engines/assets/lib/render/menu.js
+++ core/engines/assets/lib/render/menu.js
@@ -7,9 +7,11 @@ const makeRenderer = ({creatureUtils}) => {
 
 const getLabelSrc = ({label: {username}}) => {
   return `\
-    <div style="display: flex; width: ${WIDTH}px; height: ${HEIGHT}px; align-items: center;">
-      <img src="${creatureUtils.makeStaticCreature('user:' + username)}" width="${HEIGHT}" height="${HEIGHT}" style="margin-right: 30px; image-rendering: pixelated;" />
-      <span style="font-size: ${HEIGHT * 0.6}px;">${username}</span>
+    <div style="display: flex; width: ${WIDTH}px; height: ${HEIGHT}px; justify-content: center; align-items: center;">
+      <div style="display: flex; justify-content: center; align-items: center;">
+        <img src="${creatureUtils.makeStaticCreature('user:' + username)}" width="${HEIGHT}" height="${HEIGHT}" style="margin-left: -${HEIGHT}px; margin-right: 20px; image-rendering: pixelated;" />
+        <span style="font-size: ${HEIGHT * 0.6}px; white-space: nowrap; text-overflow: ellipsis;">${username}</span>
+      </div>
     </div>
   `;
 };
