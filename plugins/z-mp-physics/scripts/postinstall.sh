6bea0365f624417aeb6127b6a18d9c915028cdec
Adrian Biedrzycki <a@modules.io>
2017-04-22 19:21:50 -0700

    Replace z-mp-physics vendorized bullet lib with external npm dependency to reduce overall file size

--- plugins/z-mp-physics/scripts/postinstall.sh
+++ plugins/z-mp-physics/scripts/postinstall.sh
@@ -2,6 +2,8 @@
 
 if [ ! -f build/Release/physics.node ]; then
   cp binding.js binding.gyp
-  $(node -e 'p = require("path"); console.log(p.join(p.dirname(require.resolve("node-gyp")), "..", "bin", "node-gyp.js"))') rebuild --BULLET_PHYSICS_ROOT="$(pwd)"/bullet3;
+  "$(node -e 'p = require("path"); console.log(p.join(p.dirname(require.resolve("node-gyp")), "..", "bin", "node-gyp.js"))')" \
+    rebuild \
+    --BULLET_PHYSICS_ROOT="$(node -e 'p = require("path"); console.log(p.dirname(require.resolve("bullet3")))')"
   rm ./binding.gyp
 fi;
