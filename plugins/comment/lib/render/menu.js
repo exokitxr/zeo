a5f14c3e4c45ab3a14ef79ad06418cefdd756a73
Adrian Biedrzycki <a@modules.io>
2017-04-27 21:28:51 -0700

    Small comment plugin console.log cleanup

--- plugins/comment/lib/render/menu.js
+++ plugins/comment/lib/render/menu.js
@@ -6,8 +6,6 @@ const {
 const getCommentSrc = ({text}) => {
   text = text || ' ';
 
-  console.log('render text', {text});
-
   return `<div style="width: ${WIDTH}px; height: ${HEIGHT}px; padding: 30px; font-size: 30px;">${text}</div>`;
 };
 
