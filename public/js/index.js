37fb330038e535b0a797df2d83b2a2a2f43a99e8
Adrian Biedrzycki <a@modules.io>
2017-05-03 07:33:49 -0700

    Initial assetwallet integration

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,5 @@
 (() => {
 
-const base = document.getElementById('base');
-base.href = document.location.protocol + '//' + document.location.hostname + (document.location.port ? (':' + document.location.port) : '') + document.location.pathname + (!/\/$/.test(document.location.pathname) ? '/' : '');
-
 archae.requestPlugin('/core/engines/zeo')
   .then(() => {
     console.log('app started');
