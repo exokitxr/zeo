7ee3f77897c4767a17cf15c6518b0546a282c29c
Adrian Biedrzycki <a@modules.io>
2017-05-07 23:12:22 -0700

    Hud health bar rendering

---
+++ plugins/z-fighter/lib/render/menu.js
@@ -0,0 +1,20 @@
+const {
+  WIDTH,
+  HEIGHT,
+} = require('../constants/constants');
+
+const getHudSrc = () => {
+  return `<div style="display: flex; width: ${WIDTH}px; height: ${HEIGHT}px; color: #FFF; flex-direction: column;">
+    <div style="display: flex; font-family: Consolas, 'Liberationn Mono', Menlo, Courier, monospace; font-size: 60px; line-height: 1.4; font-weight: 600;">
+      <div style="margin-right: auto;">Health</div>
+      <div>85%</div>
+    </div>
+    <div style="position: relative; display: flex; height: 30px; width: 100%; background-color: #FFF;">
+      <div style="position: absolute; left: 0; height: 100%; width: 85%; background-color: #4CAF50;"></div>
+    </div>
+  </div>`;
+};
+
+module.exports = {
+  getHudSrc,
+};
