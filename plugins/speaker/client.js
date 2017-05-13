ffa198de4b7e5549a17473aa35c43d7d143c86e3
Adrian Biedrzycki <a@modules.io>
2017-05-13 01:49:51 -0700

    Brush up speaker model geometry

--- plugins/speaker/client.js
+++ plugins/speaker/client.js
@@ -13,9 +13,59 @@ class Speaker {
 
     const speakerGeometry = geometryUtils.concatBufferGeometry([
       new THREE.BoxBufferGeometry(0.5, 1, 0.5),
+      new THREE.BoxBufferGeometry(0.5, 0.05, 0.05)
+        .applyMatrix(new THREE.Matrix4().makeTranslation(0, (1 / 2) - (0.05 / 2), (0.5 / 2) + (0.05 / 2))),
+      new THREE.BoxBufferGeometry(0.5, 0.05, 0.05)
+        .applyMatrix(new THREE.Matrix4().makeTranslation(0, -(1 / 2) + (0.05 / 2), (0.5 / 2) + (0.05 / 2))),
+      new THREE.BoxBufferGeometry(0.05, 1, 0.05)
+        .applyMatrix(new THREE.Matrix4().makeTranslation(-(0.5 / 2) + (0.05 / 2), 0, (0.5 / 2) + (0.05 / 2))),
+      new THREE.BoxBufferGeometry(0.05, 1, 0.05)
+        .applyMatrix(new THREE.Matrix4().makeTranslation((0.5 / 2) - (0.05 / 2), 0, (0.5 / 2) + (0.05 / 2))),
+      (() => {
+        const geometry = new THREE.CylinderBufferGeometry(0.2, 0.2, 0.01, 8, 1)
+          .applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2))
+          .applyMatrix(new THREE.Matrix4().makeRotationZ(Math.PI / 8));
+
+        const _almostZero = v => Math.abs(v) < 0.001;
+
+        const {array: positions} = geometry.getAttribute('position');
+        const numPositions = positions.length / 3;
+        for (let i = 0; i < numPositions; i++) {
+          const baseIndex = i * 3;
+
+          if (_almostZero(positions[baseIndex + 0]) && _almostZero(positions[baseIndex + 1])) {
+            positions[baseIndex + 2] -= 0.1;
+          }
+        }
+
+        geometry.computeVertexNormals();
+
+        return geometry;
+      })().applyMatrix(new THREE.Matrix4().makeTranslation(0, -0.15, (0.5 / 2) + (0.1 / 2))),
+      (() => {
+        const geometry = new THREE.CylinderBufferGeometry(0.1, 0.1, 0.01, 8, 1)
+          .applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2))
+          .applyMatrix(new THREE.Matrix4().makeRotationZ(Math.PI / 8));
+
+        const _almostZero = v => Math.abs(v) < 0.001;
+
+        const {array: positions} = geometry.getAttribute('position');
+        const numPositions = positions.length / 3;
+        for (let i = 0; i < numPositions; i++) {
+          const baseIndex = i * 3;
+
+          if (_almostZero(positions[baseIndex + 0]) && _almostZero(positions[baseIndex + 1])) {
+            positions[baseIndex + 2] -= 0.05;
+          }
+        }
+
+        geometry.computeVertexNormals();
+
+        return geometry;
+      })().applyMatrix(new THREE.Matrix4().makeTranslation(0, 0.25, (0.5 / 2) + (0.05 / 2))),
     ]);
     const speakerMaterial = new THREE.MeshPhongMaterial({
-      color: 0x666666,
+      color: 0x333333,
       shininess: 10,
       shading: THREE.FlatShading,
     });
