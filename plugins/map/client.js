7c4386b918ca3444c4607f69c51797b435c4225d
Adrian Biedrzycki <a@modules.io>
2017-05-13 17:21:24 -0700

    Working on new map plugin texturing

--- plugins/map/client.js
+++ plugins/map/client.js
@@ -12,8 +12,12 @@ const SIDES = ['left', 'right'];
 class Map {
   mount() {
     const {three: {THREE, scene, camera}, elements, ui, utils: {random: randomUtils}} = zeo;
-
     const {alea} = randomUtils;
+
+    let live = true;
+    this._cleanup = () => {
+      live = false;
+    };
 
     const _decomposeObjectMatrixWorld = object => {
       const position = new THREE.Vector3();
@@ -27,130 +31,165 @@ class Map {
       rng: new alea(''),
     });
 
-    const _renderMapChunk = mapChunk => {
-      const {position, points} = mapChunk;
+    const _requestImage = url => new Promise((accept, reject) => {
+      const img = new Image();
 
-      const canvas = document.createElement('canvas');
-      canvas.width = NUM_CELLS;
-      canvas.height = NUM_CELLS;
-      canvas.position = position;
+      img.onload = () => {
+        accept(img);
+      };
+      img.onerror = err => {
+        reject(img);
+      };
 
-      const ctx = canvas.getContext('2d');
-      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-      const {data: imageDataData} = imageData;
-      for (let y = 0; y < NUM_CELLS; y++) {
-        for (let x = 0; x < NUM_CELLS; x++) {
-          const baseIndex = mapUtils.getCoordIndex(x, y);
-          const baseImageDataIndex = baseIndex * 4;
+      img.crossOrigin = 'Anonymous';
+      img.src = url;
+    });
 
-          const point = points[baseIndex];
-          const {biome} = point;
-          const colorHex = mapUtils.getBiomeColor(biome);
-          const color = new THREE.Color(colorHex);
-          imageDataData[baseImageDataIndex + 0] = color.r * 255;
-          imageDataData[baseImageDataIndex + 1] = color.g * 255;
-          imageDataData[baseImageDataIndex + 2] = color.b * 255;
-          imageDataData[baseImageDataIndex + 3] = 255;
+    return _requestImage('archae/map/img/bumpmap.jpg')
+      .then(bumpMapImg => {
+        if (live) {
+          const _renderMapChunk = mapChunk => {
+            const {position, points} = mapChunk;
+
+            const canvas = document.createElement('canvas');
+            canvas.width = NUM_CELLS;
+            canvas.height = NUM_CELLS;
+            canvas.position = position;
+
+            const ctx = canvas.getContext('2d');
+            const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            const {data: imageDataData} = imageData;
+            for (let y = 0; y < NUM_CELLS; y++) {
+              for (let x = 0; x < NUM_CELLS; x++) {
+                const baseIndex = mapUtils.getCoordIndex(x, y);
+                const baseImageDataIndex = baseIndex * 4;
+
+                const point = points[baseIndex];
+                const {biome} = point;
+                const colorHex = mapUtils.getBiomeColor(biome);
+                const color = new THREE.Color(colorHex);
+                imageDataData[baseImageDataIndex + 0] = color.r * 255;
+                imageDataData[baseImageDataIndex + 1] = color.g * 255;
+                imageDataData[baseImageDataIndex + 2] = color.b * 255;
+                imageDataData[baseImageDataIndex + 3] = 255;
+              }
+            }
+            ctx.putImageData(imageData, 0, 0);
+
+            return canvas;
+          };
+
+          const mapChunks = [
+            new THREE.Vector2(0, 0),
+          ].map(position => _renderMapChunk(mapUtils.makeMapChunk({
+            position,
+          })));
+
+          const mapComponent = {
+            selector: 'map[position]',
+            attributes: {
+              position: {
+                type: 'matrix',
+                value: [
+                  0, 1.2, 0,
+                  0, 0, 0, 1,
+                  1, 1, 1,
+                ],
+              },
+            },
+            entityAddedCallback(entityElement) {
+              const entityApi = entityElement.getComponentApi();
+              const entityObject = entityElement.getObject();
+
+              const mesh = (() => {
+                const object = new THREE.Object3D();
+
+                const meshes = mapChunks.map(canvas => {
+                  const {position} = canvas;
+
+                  const geometry = new THREE.PlaneBufferGeometry(WORLD_WIDTH, WORLD_HEIGHT)
+                    .applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
+
+                  const mapTexture = new THREE.Texture(
+                    canvas,
+                    THREE.UVMapping,
+                    THREE.ClampToEdgeWrapping,
+                    THREE.ClampToEdgeWrapping,
+                    THREE.NearestFilter,
+                    THREE.NearestFilter,
+                    THREE.RGBAFormat,
+                    THREE.UnsignedByteType,
+                    1
+                  );
+                  mapTexture.needsUpdate = true;
+
+                  const normalMapTexture = new THREE.Texture(
+                    bumpMapImg,
+                    THREE.UVMapping,
+                    THREE.RepeatWrapping,
+                    THREE.RepeatWrapping,
+                    THREE.NearestFilter,
+                    THREE.NearestFilter,
+                    THREE.RGBAFormat,
+                    THREE.UnsignedByteType,
+                    1
+                  );
+                  normalMapTexture.needsUpdate = true;
+
+                  const material = new THREE.MeshPhongMaterial({
+                    // color: 0xFFFFFF,
+                    map: mapTexture,
+                    bumpMap: mapTexture,
+                    normalMap: normalMapTexture,
+                    shininess: 10,
+                  });
+
+                  const mesh = new THREE.Mesh(geometry, material);
+                  mesh.position.set(position.x * WORLD_WIDTH, 0, position.y * WORLD_HEIGHT);
+                  return mesh;
+                });
+                meshes.forEach(mesh => {
+                  object.add(mesh);
+                });
+
+                return object;
+              })();
+              entityObject.add(mesh);
+
+              entityApi.align = () => {
+                // XXX implement this
+              };
+
+              entityApi._cleanup = () => {
+                entityObject.remove(mesh);
+              };
+            },
+            entityRemovedCallback(entityElement) {
+              const entityApi = entityElement.getComponentApi();
+
+              entityApi._cleanup();
+            },
+            entityAttributeValueChangedCallback(entityElement, name, oldValue, newValue) {
+              const entityApi = entityElement.getComponentApi();
+
+              switch (name) {
+                case 'position': {
+                  entityApi.position = newValue;
+
+                  entityApi.align();
+
+                  break;
+                }
+              }
+            },
+          };
+          elements.registerComponent(this, mapComponent);
+
+          this._cleanup = () => {
+            elements.unregisterComponent(this, mapComponent);
+          };
         }
-      }
-      ctx.putImageData(imageData, 0, 0);
-
-      return canvas;
-    };
-
-    const mapChunks = [
-      new THREE.Vector2(0, 0),
-    ].map(position => _renderMapChunk(mapUtils.makeMapChunk({
-      position,
-    })));
-
-    const mapComponent = {
-      selector: 'map[position]',
-      attributes: {
-        position: {
-          type: 'matrix',
-          value: [
-            0, 1.2, 0,
-            0, 0, 0, 1,
-            1, 1, 1,
-          ],
-        },
-      },
-      entityAddedCallback(entityElement) {
-        const entityApi = entityElement.getComponentApi();
-        const entityObject = entityElement.getObject();
-
-        const mesh = (() => {
-          const object = new THREE.Object3D();
-
-          const meshes = mapChunks.map(canvas => {
-            const {position} = canvas;
-
-            const geometry = new THREE.PlaneBufferGeometry(WORLD_WIDTH, WORLD_HEIGHT)
-              .applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
-
-            const texture = new THREE.Texture(
-              canvas,
-              THREE.UVMapping,
-              THREE.ClampToEdgeWrapping,
-              THREE.ClampToEdgeWrapping,
-              THREE.NearestFilter,
-              THREE.NearestFilter,
-              THREE.RGBAFormat,
-              THREE.UnsignedByteType,
-              1
-            );
-            texture.needsUpdate = true;
-            const material = new THREE.MeshPhongMaterial({
-              // color: 0xFFFFFF,
-              map: texture,
-              shininess: 10,
-            });
-
-            const mesh = new THREE.Mesh(geometry, material);
-            mesh.position.set(position.x * WORLD_WIDTH, 0, position.y * WORLD_HEIGHT);
-            return mesh;
-          });
-          meshes.forEach(mesh => {
-            object.add(mesh);
-          });
-
-          return object;
-        })();
-        entityObject.add(mesh);
-
-        entityApi.align = () => {
-          // XXX implement this
-        };
-
-        entityApi._cleanup = () => {
-          entityObject.remove(mesh);
-        };
-      },
-      entityRemovedCallback(entityElement) {
-        const entityApi = entityElement.getComponentApi();
-
-        entityApi._cleanup();
-      },
-      entityAttributeValueChangedCallback(entityElement, name, oldValue, newValue) {
-        const entityApi = entityElement.getComponentApi();
-
-        switch (name) {
-          case 'position': {
-            entityApi.position = newValue;
-
-            entityApi.align();
-
-            break;
-          }
-        }
-      },
-    };
-    elements.registerComponent(this, mapComponent);
-
-    this._cleanup = () => {
-      elements.unregisterComponent(this, mapComponent);
-    };
+      });
   }
 
   unmount() {
--- plugins/map/lib/constants/map.js
+++ plugins/map/lib/constants/map.js
@@ -1,7 +1,7 @@
 export const HEIGHT = 128;
 export const ASPECT_RATIO = 1.5;
 export const WIDTH = HEIGHT * ASPECT_RATIO;
-export const WORLD_WIDTH = 128;
+export const WORLD_WIDTH = 32;
 export const WORLD_HEIGHT = WORLD_WIDTH;
 export const NUM_CELLS = HEIGHT;
 
