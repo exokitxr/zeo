0ff91bcc86d949a97b0d331f423ca73ff8ff857a
Adrian Biedrzycki <a@modules.io>
2017-05-16 07:11:58 -0700

    Clean up home engine videos rendering

--- core/engines/home/lib/render/menu.js
+++ core/engines/home/lib/render/menu.js
@@ -33,130 +33,47 @@ const getHomeMenuSrc = ({page, vrMode, videos}) => {
 };
 
 const getTutorialPageSrc = (pageIndex, vrMode) => {
-  const keyboardVrMode = vrMode === null || vrMode === 'keyboard';
+  // const keyboardVrMode = vrMode === null || vrMode === 'keyboard';
 
   const content = (() => {
     switch (pageIndex) {
       case 0: return `\
         <div style="display: flex; padding: 0 50px; justify-content: center; align-items: center; flex-direction: column; flex-grow: 1;">
           <div style="display: flex; width: 100%; height: 100px; margin-top: auto; justify-content: center; align-items: center;">
-            <!-- <div style="width: 200px;"></div>
-            <div style="display: flex; flex-grow: 1;">
-              <a style="display: flex; width: 90px; height: 100px; justify-content: center; align-items: center;" onclick="tutorial:setPage:1">
-                <div style="display: flex; width: 10px; height: 10px; background-color: #000;"></div>
-              </a>
-              <a style="display: flex; width: 90px; height: 100px; justify-content: center; align-items: center;" onclick="tutorial:setPage:2">
-                <div style="display: flex; width: 10px; height: 10px; background-color: #CCC;"></div>
-              </a>
-              <a style="display: flex; width: 90px; height: 100px; justify-content: center; align-items: center;" onclick="tutorial:setPage:3">
-                <div style="display: flex; width: 10px; height: 10px; background-color: #CCC;"></div>
-              </a>
-              <a style="display: flex; width: 90px; height: 100px; justify-content: center; align-items: center;" onclick="tutorial:setPage:4">
-                <div style="display: flex; width: 10px; height: 10px; background-color: #CCC;"></div>
-              </a>
-              <a style="display: flex; width: 90px; height: 100px; justify-content: center; align-items: center;" onclick="tutorial:setPage:5">
-                <div style="display: flex; width: 10px; height: 10px; background-color: #CCC;"></div>
-              </a>
-            </div>
-            <div style="display: flex; width: 100%"> -->
-              <a style="display: flex; margin-left: auto; margin-right: 40px; font-size: 20px; font-weight: 400; text-decoration: none;" onclick="home:skipAll">Skip all tutorials</a>
-              <a style="display: flex; padding: 10px 15px; border: 2px solid; font-size: 20px; font-weight: 400; text-decoration: none;" onclick="home:next">Next: Modules</a>
-            <!-- </div> -->
+            <a style="display: flex; margin-left: auto; margin-right: 40px; font-size: 20px; font-weight: 400; text-decoration: none;" onclick="home:skipAll">Skip all tutorials</a>
+            <a style="display: flex; padding: 10px 15px; border: 2px solid; font-size: 20px; font-weight: 400; text-decoration: none;" onclick="home:next">Next: Modules</a>
           </div>
         </div>
       `;
       case 1: return `\
        <div style="display: flex; padding: 0 50px; justify-content: center; align-items: center; flex-direction: column; flex-grow: 1;">
           <div style="display: flex; width: 100%; height: 100px; margin-top: auto; justify-content: center; align-items: center;">
-            <!-- <div style="font-size: 30px; font-weight: 400;">Discover your superpowers</div>
-            <div style="display: flex;">
-              <img src="" width="256" height="128" style="margin: 10px 0; margin-right: 28px;" />
-              <img src="" width="256" height="128" style="margin: 10px 0;" />
-            </div>
-            <div style="width: 540px; margin-bottom: auto; font-size: 15px; font-weight: 400; flex-grow: 1">
-              <p>This screen is the <b>MENU</b>. The menu has tools to edit your VR world, move between worlds, and change settings. It's showing you this tutorial.</p>
-              <p>To <b>OPEN</b> or <b>CLOSE</b> the menu, press the <b>MENU</b> the <b>PAD${keyboardVrMode ? ' (E key)' : ''}</b> on your controller.</p>
-              <p>To <b>TELEPORT</b> around the world, <b>HOLD</b> the <b>PAD${keyboardVrMode ? ' (Q key)' : ''}</b> on to target and <b>RELEASE</b> to go there. Use your finger to adjust how far you'll teleport.</p>
-              <p style="margin-bottom: 0; font-size: 18px;">
-                <i>
-                  <i>To continue, click the <b>NEXT BUTTON</b> with your <b>TRIGGER</b>:</i>
-                </i>
-              </p>
-              </p>
-            </div>
-            <div style="display: flex; width: 100%;"> -->
-              <a style="display: flex; margin-left: auto; margin-right: 40px; font-size: 20px; font-weight: 400; text-decoration: none;" onclick="home:skipAll">Skip all tutorials</a>
-              <a style="display: flex; padding: 10px 15px; border: 2px solid; font-size: 20px; font-weight: 400; text-decoration: none;" onclick="home:next">Next: Servers</a>
-            <!-- </div> -->
+            <a style="display: flex; margin-left: auto; margin-right: 40px; font-size: 20px; font-weight: 400; text-decoration: none;" onclick="home:skipAll">Skip all tutorials</a>
+            <a style="display: flex; padding: 10px 15px; border: 2px solid; font-size: 20px; font-weight: 400; text-decoration: none;" onclick="home:next">Next: Servers</a>
           </div>
         </div>
       `;
       case 2: return `\
         <div style="display: flex; padding: 0 50px; justify-content: center; align-items: center; flex-direction: column; flex-grow: 1;">
           <div style="display: flex; width: 100%; height: 100px; margin-top: auto; justify-content: center; align-items: center;">
-            <!-- <div style="font-size: 30px; font-weight: 400;">The cake is real</div>
-            <img src="" width="256" height="128" style="margin: 10px 0;" />
-            <div style="width: 540px; margin-bottom: auto; font-size: 15px; font-weight: 400; flex-grow: 1">
-              <p>In Zeo VR, your world is made up of <i>modules</i>. Modules are objects you can add to the world.</p>
-              <p>For example, here is a <b>CAKE MODULE</b>:</p>
-              <div style="width: 100px; height: 100px;"></div>
-              <p style="margin-bottom: 0; font-size: 18px;">
-                <i>
-                  To continue, <b>ADD</b> the cake to the world and <b>EAT</b> it.<br/>
-                  Grab a slice by holding the <b>GRIP (F key)</b> and move it to your mouth.<br/>
-                </i>
-              </p>
-            </div>
-            <div style="display: flex; width: 100%;"> -->
-              <a style="display: flex; margin-left: auto; margin-right: 40px; font-size: 20px; font-weight: 400; text-decoration: none;" onclick="home:skipAll">Skip all tutorials</a>
-              <a style="display: flex; padding: 10px 15px; border: 2px solid; font-size: 20px; font-weight: 400; text-decoration: none;" onclick="home:next">Next: Multiplayer</a>
-            <!-- </div> -->
+            <a style="display: flex; margin-left: auto; margin-right: 40px; font-size: 20px; font-weight: 400; text-decoration: none;" onclick="home:skipAll">Skip all tutorials</a>
+            <a style="display: flex; padding: 10px 15px; border: 2px solid; font-size: 20px; font-weight: 400; text-decoration: none;" onclick="home:next">Next: Multiplayer</a>
           </div>
         </div>
       `;
       case 3: return `\
         <div style="display: flex; padding: 0 50px; justify-content: center; align-items: center; flex-direction: column; flex-grow: 1;">
           <div style="display: flex; width: 100%; height: 100px; margin-top: auto; justify-content: center; align-items: center;">
-            <!-- <div style="font-size: 30px; font-weight: 400;">It's dangerous to go alone!</div>
-            <img src="" width="256" height="128" style="margin: 10px 0;" />
-            <div style="width: 540px; margin-bottom: auto; font-size: 15px; font-weight: 400; flex-grow: 1">
-              <p>Zeo VR lets you connect to multiplayer world servers.</p>
-              <p>Look at the <b>LINK ORBS</b> around you. Each Link Orb is a server you can join. To connect to a server, <b>POINT</b> at it and click your <b>TRIGGER</b>.</p>
-              <p>Some servers are <b>LOCKED</b> until you get permission from the owner. Contact info for each server is written above the server, but you can <i>sneak a peek</i> through the orb.</p>
-              <p style="margin-bottom: 0; font-size: 18px;">
-                <i>
-                  To <b>LEARN</b> how to code your own worlds, read the <a onclick="home:apiDocs"><b>API Docs</b></a>.<br/>
-                  To <b>HIDE</b> the tutorial, click the <b>NEXT BUTTON</b>.<br/>
-                </i>
-              </p>
-            </div>
-            <div style="display: flex; width: 100%;"> -->
-              <a style="display: flex; margin-left: auto; margin-right: 40px; font-size: 20px; font-weight: 400; text-decoration: none;" onclick="home:skipAll">Skip all tutorials</a>
-              <a style="display: flex; padding: 10px 15px; border: 2px solid; font-size: 20px; font-weight: 400; text-decoration: none;" onclick="home:next">Next: Host your own</a>
-            <!-- </div> -->
+            <a style="display: flex; margin-left: auto; margin-right: 40px; font-size: 20px; font-weight: 400; text-decoration: none;" onclick="home:skipAll">Skip all tutorials</a>
+            <a style="display: flex; padding: 10px 15px; border: 2px solid; font-size: 20px; font-weight: 400; text-decoration: none;" onclick="home:next">Next: Host your own</a>
           </div>
         </div>
       `;
       case 4: return `\
         <div style="display: flex; padding: 0 50px; justify-content: center; align-items: center; flex-direction: column; flex-grow: 1;">
           <div style="display: flex; width: 100%; height: 100px; margin-top: auto; justify-content: center; align-items: center;">
-            <!-- <div style="font-size: 30px; font-weight: 400;">It's dangerous to go alone!</div>
-            <img src="" width="256" height="128" style="margin: 10px 0;" />
-            <div style="width: 540px; margin-bottom: auto; font-size: 15px; font-weight: 400; flex-grow: 1">
-              <p>Zeo VR lets you connect to multiplayer world servers.</p>
-              <p>Look at the <b>LINK ORBS</b> around you. Each Link Orb is a server you can join. To connect to a server, <b>POINT</b> at it and click your <b>TRIGGER</b>.</p>
-              <p>Some servers are <b>LOCKED</b> until you get permission from the owner. Contact info for each server is written above the server, but you can <i>sneak a peek</i> through the orb.</p>
-              <p style="margin-bottom: 0; font-size: 18px;">
-                <i>
-                  To <b>LEARN</b> how to code your own worlds, read the <a onclick="home:apiDocs"><b>API Docs</b></a>.<br/>
-                  To <b>HIDE</b> the tutorial, click the <b>NEXT BUTTON</b>.<br/>
-                </i>
-              </p>
-            </div>
-            <div style="display: flex; width: 100%;"> -->
             <a style="display: flex; margin-left: auto; margin-right: 40px; font-size: 20px; font-weight: 400; text-decoration: none;" onclick="home:skipAll">Skip all tutorials</a>
   <a style="display: flex; padding: 10px 15px; border: 2px solid; font-size: 20px; font-weight: 400; text-decoration: none;" onclick="home:next">Go to main menu</a>
-            <!-- </div> -->
           </div>
         </div>
       `;
@@ -182,7 +99,7 @@ const getVideosPageSrc = videos => {
     <div style="display: flex; flex-direction: column; flex-grow: 1;">
       <div style="display: flex; margin-bottom: auto; flex-direction: column;">
         ${videos.map((video, index) =>
-          `<a style="display: flex; padding: 10px 0; margin: 0 50px; text-decoration: none; align-items: center;" onclick="home:video:${index}">
+          `<a style="display: flex; margin-right: 150px; padding: 10px 30px; padding-right: 0; text-decoration: none; align-items: center;" onclick="home:video:${index}">
              <img src="${video.thumbnailImgData}" style="display: block; height: 60px; width: ${60 * 1.5}px; margin-right: 20px;">
              <div style="display: flex; height: 60px; font-size: 24px; font-weight: 400;">${video.name}</div>
           </a>`
