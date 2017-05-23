27b930b8821d9c52c975f82d8d9a8a9213a45104
Adrian Biedrzycki <a@modules.io>
2017-05-22 18:48:46 -0700

    Glass hud highlight support

--- plugins/glass/client.js
+++ plugins/glass/client.js
@@ -15,7 +15,7 @@ const SIDES = ['left', 'right'];
 
 class Glass {
   mount() {
-    const {three: {THREE, scene, camera}, pose, render, ui} = zeo;
+    const {three: {THREE, scene, camera}, pose, input, render, ui} = zeo;
 
     const _decomposeObjectMatrixWorld = object => _decomposeMatrix(object.matrixWorld);
     const _decomposeMatrix = matrix => {
@@ -26,9 +26,15 @@ class Glass {
       return {position, rotation, scale};
     };
 
-    const glassState = {
+    const globalGlassState = {
       mode: 'picture',
+    };
+    const _makeGlassState = () => ({
       highlight: 'audio',
+    });
+    const glassStates = {
+      left: _makeGlassState(),
+      right: _makeGlassState(),
     };
 
     const hudMesh = (() => {
@@ -38,21 +44,25 @@ class Glass {
         color: [1, 1, 1, 0],
       });
       const mesh = menuUi.makePage(({
-        glass: {
-          mode,
-          highlight,
-        },
-      }) => ({
-        type: 'html',
-        src: menuRenderer.getHudSrc({mode, highlight}),
-        x: 0,
-        y: 0,
-        w: WIDTH,
-        h: HEIGHT,
-      }), {
+        globalGlassState,
+        glassStates,
+      }) => {
+        const {mode} = globalGlassState;
+        const highlights = [glassStates.left.highlight, glassStates.right.highlight];
+
+        return {
+          type: 'html',
+          src: menuRenderer.getHudSrc({mode, highlights}),
+          x: 0,
+          y: 0,
+          w: WIDTH,
+          h: HEIGHT,
+        };
+      }, {
         type: 'glass',
         state: {
-          glass: glassState,
+          globalGlassState,
+          glassStates,
         },
         worldWidth: WORLD_WIDTH,
         worldHeight: WORLD_HEIGHT,
@@ -94,13 +104,53 @@ class Glass {
     })();
     scene.add(hudMesh);
 
+    const highlightSpecs = [
+      {
+        mode: 'picture',
+        x: 0.25,
+        y: 0.25,
+        size: 0.25,
+      },
+      {
+        mode: 'audio',
+        x: 0.25,
+        y: 0,
+        size: 0.25,
+      },
+      {
+        mode: 'video',
+        x: 0.25,
+        y: -0.25,
+        size: 0.25,
+      },
+    ];
+
+    const _trigger = e => {
+      const {side} = e;
+      const glassState = glassStates[side];
+      const {highlight} = glassState;
+
+      if (highlight !== null) {
+        const {mode: oldMode} = globalGlassState;
+        const newMode = highlight;
+
+        if (newMode !== oldMode) {
+          globalGlassState.mode = newMode;
+
+          const {page} = hudMesh;
+          page.update();
+        }
+      }
+    };
+    input.on('trigger', _trigger);
+
     let now = Date.now();
     let lastUpdateTime = now;
     const _update = () => {
       now = Date.now();
 
+      const {position: cameraPosition, rotation: cameraRotation, scale: cameraScale} = _decomposeObjectMatrixWorld(camera);
       const _updateHudMesh = () => {
-        const {position: cameraPosition, rotation: cameraRotation, scale: cameraScale} = _decomposeObjectMatrixWorld(camera);
         const timeDiff = now - lastUpdateTime;
         const lerpFactor = timeDiff * 0.005;
         hudMesh.align(cameraPosition, cameraRotation, cameraScale, lerpFactor);
@@ -109,14 +159,68 @@ class Glass {
           hudMesh.visible = true;
         }
       };
+      const _updateHover = () => {
+        const {gamepads} = pose.getStatus();
+
+        SIDES.forEach(side => {
+          const gamepad = gamepads[side];
+
+          if (gamepad) {
+            const {worldPosition: controllerPosition} = gamepad;
+            const glassState = glassStates[side];
+
+            const distanceSpecs = highlightSpecs.map(highlightSpec => {
+              const {mode, x, y, size} = highlightSpec;
+              const highlightPosition = cameraPosition.clone()
+                .add(new THREE.Vector3(x, y).applyQuaternion(cameraRotation));
+              const distance = controllerPosition.distanceTo(highlightPosition);
+
+              if (distance <= size) {
+                return {
+                  mode,
+                  distance,
+                };
+              } else {
+                return null;
+              }
+            }).filter(distanceSpec => distanceSpec !== null);
+
+            if (distanceSpecs.length > 0) {
+              const distanceSpec = distanceSpecs.sort((a, b) => a.distance - b.distance)[0];
+
+              const {highlight: oldHighlight} = glassState;
+              const {mode: newHighlight} = distanceSpec;
+
+              if (newHighlight !== oldHighlight) {
+                glassState.highlight = newHighlight;
+
+                const {page} = hudMesh;
+                page.update();
+              }
+            } else {
+              const {highlight: oldHighlight} = glassState;
+              const newHighlight = null;
+
+              if (newHighlight !== oldHighlight) {
+                glassState.highlight = null;
+
+                const {page} = hudMesh;
+                page.update();
+              }
+            }
+          }
+        });
+      };
 
       _updateHudMesh();
+      _updateHover();
     };
     render.on('update', _update);
 
     this._cleanup = () => {
       scene.remove(hudMesh);
 
+      input.removeListener('trigger', _trigger);
       render.removeListener('update', _update);
     };
   }
--- plugins/glass/lib/render/menu.js
+++ plugins/glass/lib/render/menu.js
@@ -3,10 +3,10 @@ const {
   HEIGHT,
 } = require('../constants/constants');
 
-const getHudSrc = ({mode, highlight}) => {
+const getHudSrc = ({mode, highlights}) => {
   return `<div style="display: flex; position: relative; width: ${WIDTH}px; height: ${HEIGHT}px; color: #FFF; flex-direction: column;">
     <div style="position: absolute; top: 20px; right: 20px; display: flex; font-family: Consolas, 'Liberation Mono', Menlo, Courier, monospace; font-size: 60px; line-height: 1.4; font-weight: 600; flex-direction: column;">
-      <div style="display: flex; padding: 0 10px; ${highlight === 'picture' ? 'border: 2px solid;' : ''}; justify-content: center; align-items: center;">
+      <div style="display: flex; padding: 0 10px; border: 2px solid transparent; ${(highlights.indexOf('picture') !== -1) ? 'border-color: #FFF' : ''}; justify-content: center; align-items: center;">
         ${mode === 'picture' ? `\
           <div style="width: 40px; height: 40px; margin-right: 30px; background-color: #FF0000; border-radius: 100px;"></div>
         ` : `\
@@ -14,7 +14,7 @@ const getHudSrc = ({mode, highlight}) => {
         `}
         <div style="margin-right: auto;">Picture</div>
       </div>
-      <div style="display: flex; padding: 0 10px; ${highlight === 'audio' ? 'border: 2px solid;' : ''}; justify-content: center; align-items: center;">
+      <div style="display: flex; padding: 0 10px; border: 2px solid transparent; ${(highlights.indexOf('audio') !== -1) ? 'border-color: #FFF' : ''}; justify-content: center; align-items: center;">
         ${mode === 'audio' ? `\
           <div style="width: 40px; height: 40px; margin-right: 30px; background-color: #FF0000; border-radius: 100px;"></div>
         ` : `\
@@ -22,7 +22,7 @@ const getHudSrc = ({mode, highlight}) => {
         `}
         <div style="margin-right: auto;">Audio</div>
       </div>
-      <div style="display: flex; padding: 0 10px; ${highlight === 'video' ? 'border: 2px solid;' : ''}; justify-content: center; align-items: center;">
+      <div style="display: flex; padding: 0 10px; border: 2px solid transparent; ${(highlights.indexOf('video') !== -1) ? 'border-color: #FFF' : ''}; justify-content: center; align-items: center;">
         ${mode === 'video' ? `\
           <div style="width: 40px; height: 40px; margin-right: 30px; background-color: #FF0000; border-radius: 100px;"></div>
         ` : `\
