019c55a3cbb4a1e33cf1bce05efa392c691ddcdf
Adrian Biedrzycki <a@modules.io>
2017-06-04 15:48:07 -0700

    Rend engine update matrix world

--- core/engines/rend/client.js
+++ core/engines/rend/client.js
@@ -229,6 +229,7 @@ class Rend {
           return object;
         })();
         scene.add(menuMesh);
+        menuMesh.updateMatrixWorld();
 
         const trigger = e => {
           const {side} = e;
