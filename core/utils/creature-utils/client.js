4cf4f14ce4643c8e213003be7c5986363a240176
Adrian Biedrzycki <a@modules.io>
2017-06-09 00:05:39 -0700

    Update creature utils with canvas output support

--- core/utils/creature-utils/client.js
+++ core/utils/creature-utils/client.js
@@ -1,12 +1,14 @@
 const creature = require('creaturejs');
 
+window.creature = creature;
+
 const creatureUtils = () => ({
   mount() {
-    const {makeAnimatedCreature, makeStaticCreature} = creature;
-
+    const {makeAnimatedCreature, makeStaticCreature, makeCanvasCreature} = creature;
     return {
       makeAnimatedCreature,
       makeStaticCreature,
+      makeCanvasCreature,
     };
   },
 });
