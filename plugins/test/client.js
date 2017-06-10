e72abee31de27d2ae6945661fd172ea06dd263b6
Adrian Biedrzycki <a@modules.io>
2017-06-10 10:54:50 -0700

    Add in-vr payments test support to test plugin

--- plugins/test/client.js
+++ plugins/test/client.js
@@ -1,6 +1,6 @@
 class Test {
   mount() {
-    const {elements, input, payment} = zeo;
+    const {elements, input, pose, payment} = zeo;
 
     console.log('mount');
 
@@ -59,7 +59,7 @@ class Test {
     };
     elements.registerComponent(this, testComponent);
 
-    const keypress = e => {
+    const _keypress = e => {
       if (e.keyCode === 98) { // B
         payment.requestBuy({
           srcAsset: 'CRAPCOIN',
@@ -87,12 +87,37 @@ class Test {
           });
       }
     };
-    input.on('keypress', keypress);
+    input.on('keypress', _keypress);
+    const _menudown = e => {
+      const {side} = e;
+      const {gamepads} = pose.getStatus();
+      const gamepad = gamepads[side];
+      const {buttons: {grip: {pressed: gripPressed}}} = gamepad;
+
+      if (gripPressed) {
+        payment.requestPay({
+          address: 'n3W1ExUh7Somt28Qe7DT5FUfY127MY4r1X',
+          asset: 'CRAPCOIN',
+          quantity: 10,
+        })
+          .then(result => {
+            console.warn('pay result', result);
+          })
+          .catch(err => {
+            console.warn('pay error', err);
+          });
+
+        e.stopImmediatePropagation();
+      }
+    };
+    input.on('menudown', _menudown, {
+      priority: 1,
+    });
 
     this._cleanup = () => {
       elements.unregisterComponent(this, testComponent);
 
-      input.removeListener('keypress', keypress);
+      input.removeListener('keypress', _keypress);
     };
   }
 
