d828e42a4f9c148b8738fab412d8a74bb3714af5
Adrian Biedrzycki <a@modules.io>
2017-06-19 17:33:00 -0700

    Add windows release build script

---
+++ scripts/build-windows.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pushd "$DIR"/..;
+
+curl https://nodejs.org/dist/latest/node-v8.1.2-win-x64.zip >node.zip
+unzip node.zip
+rm node.zip
+mv node-v8.1.2-win-x64 node
+cp node/node_modules/npm/bin/npm.cmd node/
+zip -r windows-release.zip . -x /.git* -x /data*
+rm -R node
+
+popd;
