a953c5ccd3e14458e538a64b9f1a982d57c1e98b
Adrian Biedrzycki <a@modules.io>
2017-06-21 18:59:55 -0700

    Update windows start script

--- scripts/standalone.ps1
+++ scripts/standalone.ps1
@@ -8,6 +8,7 @@ node "$indexJs" server \
   port="$PORT" \
   siteUrl='https://zeovr.io' \
   hubUrl='https://hub.zeovr.io:8000' \
+  vridUrl='https://zeovr.io' \
   dataDirectorySrc='defaults/data' \
   cryptoDirectorySrc='defaults/crypto' \
   installDirectorySrc='installed'
