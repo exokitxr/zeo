0eaacecd8c05d1b0475b4479ad7be9e25a94a46e
Adrian Biedrzycki <a@modules.io>
2017-06-23 03:47:47 -0700

    Windows build script cleanup

--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -9,7 +9,9 @@ unzip node.zip
 rm node.zip
 mv node-v8.1.2-win-x64 node
 cp node/node_modules/npm/bin/npm.cmd node/
+cp 'scripts/lib/windows/Zeo VR.lnk' .
 zip -r windows-release.zip . -x /.git* -x /data*
 rm -R node
+rm 'Zeo VR.lnk'
 
 popd;
