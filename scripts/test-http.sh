e36dade9a4b4a4836ce230b34e49d5462cb0e5d8
Adrian Biedrzycki <a@modules.io>
2017-06-23 21:37:40 -0700

    Remove worldname arg from start scripts

--- scripts/test-http.sh
+++ scripts/test-http.sh
@@ -9,7 +9,6 @@ pushd "$DIR"/../
   siteUrl='https://127.0.0.1:8080' \
   homeUrl='http://127.0.0.1:8081' \
   vridUrl='https://127.0.0.1:8080' \
-  worldname='server_one' \
   dataDirectory='data/servers/server_one/data' dataDirectorySrc='defaults/data' \
   cryptoDirectory='data/servers/server_one/crypto' cryptoDirectorySrc='crypto' \
   installDirectory='data/servers/server_one/installed' &
@@ -18,7 +17,6 @@ pushd "$DIR"/../
   siteUrl='https://127.0.0.1:8080' \
   homeUrl='http://127.0.0.1:8081' \
   vridUrl='https://127.0.0.1:8080' \
-  worldname='server_two' \
   dataDirectory='data/servers/server_two/data' dataDirectorySrc='defaults/data' \
   cryptoDirectory='data/servers/server_two/crypto' cryptoDirectorySrc='crypto' \
   installDirectory='data/servers/server_two/installed' &
