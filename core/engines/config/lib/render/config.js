3bc7b8750ce78bd41ff4ae77e9c30584de7ac253
Adrian Biedrzycki <a@modules.io>
2017-06-23 21:25:02 -0700

    Add config page server name setting

--- core/engines/config/lib/render/config.js
+++ core/engines/config/lib/render/config.js
@@ -2,22 +2,22 @@ const {
   WIDTH,
 } = require('../constants/config');
 
-const getConfigPageSrc = ({resolutionValue, voiceChatCheckboxValue, statsCheckboxValue, visibilityValue, passwordValue, maxPlayersValue, inputValue, focusSpec, flags}) => {
+const getConfigPageSrc = ({resolutionValue, voiceChatCheckboxValue, statsCheckboxValue, visibilityValue, nameValue, passwordValue, maxPlayersValue, inputValue, focusSpec}) => {
   const focusVisibility = Boolean(focusSpec) && focusSpec.type === 'visibility';
+  const focusName = Boolean(focusSpec) && focusSpec.type === 'name';
   const focusPassword = Boolean(focusSpec) && focusSpec.type === 'password';
 
   return `<div style="width: ${WIDTH}px;">
     <div style="display: flex; width: 640px; padding: 0 30px; box-sizing: border-box; flex-direction: column;">
       <h1 style="margin: 15px 0; font-size: 40px; font-weight: 400;">Browser settings</h1>
       ${getSliderSrc('Resolution', resolutionValue, 'config:resolution')}
-      ${flags.server ? getCheckboxSrc('Voice chat', voiceChatCheckboxValue, 'config:voiceChat') : ''}
+      ${getCheckboxSrc('Voice chat', voiceChatCheckboxValue, 'config:voiceChat')}
       ${getCheckboxSrc('Stats', statsCheckboxValue, 'config:stats')}
-      ${flags.server ? `\
-        <h1 style="margin: 15px 0; font-size: 40px; font-weight: 400;">Server settings</h1>
-        ${getSelectSrc('Visibility', visibilityValue, ['public', 'private'], focusVisibility, 'config:visibility')}
-        ${getInputSrc('Password', passwordValue, 'Enter password', inputValue, focusPassword, 'config:password')}
-        ${getSliderSrc('Max players', (maxPlayersValue - 1) / (8 - 1), 'config:maxPlayers')}
-      ` : ''}
+      <h1 style="margin: 15px 0; font-size: 40px; font-weight: 400;">Server settings</h1>
+      ${getInputSrc('Name', nameValue, 'Enter server name', inputValue, focusName, 'config:name')}
+      ${getSelectSrc('Visibility', visibilityValue, ['public', 'private'], focusVisibility, 'config:visibility')}
+      ${getInputSrc('Password', passwordValue, 'Enter password', inputValue, focusPassword, 'config:password')}
+      ${getSliderSrc('Max players', (maxPlayersValue - 1) / (8 - 1), 'config:maxPlayers')}
     </div>
   </div>`;
 };
