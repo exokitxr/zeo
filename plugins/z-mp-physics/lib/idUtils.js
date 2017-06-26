c1cd6b7d7ad9ae6629d2497b7867719855514b45
Adrian Biedrzycki <a@modules.io>
2017-06-26 12:57:03 -0700

    Remove old bullet multiplayer physics plugin

--- plugins/z-mp-physics/lib/idUtils.js
+++
@@ -1,6 +0,0 @@
-const api = {};
-
-const makeId = () => Math.random().toString(36).substring(7);
-api.makeId = makeId;
-
-module.exports = api;
--- plugins/z-mp-physics/scripts/postinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -f build/Release/physics.node ]; then
-  cp binding.js binding.gyp
-  "$(node -e 'p = require("path"); console.log(p.join(p.dirname(require.resolve("node-gyp")), "..", "bin", "node-gyp.js"))')" \
-    rebuild \
-    --BULLET_PHYSICS_ROOT="$(node -e 'p = require("path"); console.log(p.dirname(require.resolve("bullet3")))')"
-  rm ./binding.gyp
-fi;
