ba56cf89f92c514c4b51aa55ecea2c2cb8d77ba7
Adrian Biedrzycki <a@modules.io>
2017-06-30 03:41:45 -0700

    Grass plugin now streams chunks from the backend

--- plugins/grass/client.js
+++ plugins/grass/client.js
@@ -37,156 +37,161 @@ class Grass {
         return Promise.reject(err);
       }
     };
-    const _requestGrassTemplates = () => fetch('archae/grass/templates')
-      .then(_resArrayBuffer)
-      .then(grassTemplatesBuffer => protocolUtils.parseGrassGeometry(grassTemplatesBuffer))
-      .then(grassTemplateSpec => {
-        const {positions, colors} = grassTemplateSpec;
-
-        const geometry = new THREE.BufferGeometry();
-        geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3));
-        geometry.addAttribute('color', new THREE.BufferAttribute(colors, 3));
-        return geometry;
-      });
     const _requestGrassGenerate = (x, y) => fetch(`archae/grass/generate?x=${x}&y=${y}`)
       .then(_resArrayBuffer)
-      .then(grassPostionsBuffer => {
-        return new Float32Array(grassPostionsBuffer);
-      });
-    const _copyIndices = (src, dst, startIndexIndex, startAttributeIndex) => {
+      .then(grassChunkBuffer => protocolUtils.parseGrassGeometry(grassChunkBuffer));
+    /* const _copyIndices = (src, dst, startIndexIndex, startAttributeIndex) => {
       for (let i = 0; i < src.length; i++) {
         dst[startIndexIndex + i] = src[i] + startAttributeIndex;
       }
+    }; */
+
+    const _makeGrassChunkMesh = mapChunkData => {
+      const {positions, colors} = mapChunkData;
+
+      const geometry = (() => {
+        let geometry = new THREE.BufferGeometry();
+        geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3));
+        geometry.addAttribute('color', new THREE.BufferAttribute(colors, 3));
+
+        return geometry;
+      })();
+      const material = grassMaterial;
+
+      const mesh = new THREE.Mesh(geometry, material);
+      // mesh.frustumCulled = false;
+      mesh.drawMode = THREE.TriangleStripDrawMode;
+
+      mesh.destroy = () => {
+        geometry.dispose();
+      };
+
+      return mesh;
     };
 
-    return _requestGrassTemplates()
-      .then(grassGeometry => {
-        if (live) {
-          const chunker = chnkr.makeChunker({
-            resolution: 32,
-            lods: 1,
+    const chunker = chnkr.makeChunker({
+      resolution: 32,
+      lods: 1,
+    });
+
+    /* const _makeGrassMesh = grassPositions => {
+      const positions = new Float32Array(NUM_POSITIONS * 3);
+      const colors = new Float32Array(NUM_POSITIONS * 3);
+      let attributeIndex = 0;
+
+      const position = new THREE.Vector3();
+      const quaternion = new THREE.Quaternion();
+      const scale = new THREE.Vector3(1, 1, 1);
+      const matrix = new THREE.Matrix4();
+
+      const numGrassPositions = grassPositions.length / 3;
+      for (let i = 0; i < numGrassPositions; i++) {
+        const baseIndex = i * 3;
+        position.set(
+          grassPositions[baseIndex + 0],
+          grassPositions[baseIndex + 1],
+          grassPositions[baseIndex + 2]
+        );
+        quaternion.setFromAxisAngle(upVector, Math.random() * Math.PI * 2);
+        matrix.compose(position, quaternion, scale);
+        const geometry = grassGeometry
+          .clone()
+          .applyMatrix(matrix);
+        const newPositions = geometry.getAttribute('position').array;
+        positions.set(newPositions, attributeIndex);
+        const newColors = geometry.getAttribute('color').array;
+        colors.set(newColors, attributeIndex);
+
+        attributeIndex += newPositions.length;
+      }
+      const geometry = new THREE.BufferGeometry();
+      geometry.addAttribute('position', new THREE.BufferAttribute(new Float32Array(positions.buffer, positions.byteOffset, attributeIndex), 3));
+      geometry.addAttribute('color', new THREE.BufferAttribute(new Float32Array(colors.buffer, colors.byteOffset, attributeIndex), 3));
+
+      const material = grassMaterial;
+
+      const mesh = new THREE.Mesh(geometry, material);
+      // mesh.position.y = 0.5;
+      // mesh.updateMatrixWorld();
+      // mesh.frustumCulled = false;
+      mesh.drawMode = THREE.TriangleStripDrawMode;
+
+      mesh.destroy = () => {
+        geometry.dispose();
+      };
+
+      return mesh;
+    }; */
+
+    const _requestRefreshGrassChunks = () => {
+      const {hmd} = pose.getStatus();
+      const {worldPosition: hmdPosition} = hmd;
+      const {added, removed} = chunker.update(hmdPosition.x, hmdPosition.z);
+
+      const addedPromises = added.map(chunk => {
+        const {x, z} = chunk;
+
+        return _requestGrassGenerate(x, z)
+          .then(grassChunkData => {
+            const grassChunkMesh = _makeGrassChunkMesh(grassChunkData);
+            scene.add(grassChunkMesh);
+
+            chunk.data = grassChunkMesh;
           });
+      });
+      return Promise.all(addedPromises)
+        .then(() => {
+          removed.forEach(chunk => {
+            const {data: grassChunkMesh} = chunk;
+            scene.remove(grassChunkMesh);
+            grassChunkMesh.destroy();
+          });
+        })
+    };
 
-          const _makeGrassMesh = grassPositions => {
-            const positions = new Float32Array(NUM_POSITIONS * 3);
-            const colors = new Float32Array(NUM_POSITIONS * 3);
-            let attributeIndex = 0;
+    return _requestRefreshGrassChunks()
+      .then(() => {
+        let updating = false;
+        let updateQueued = false;
+        const tryGrassChunkUpdate = () => {
+          if (!updating) {
+            updating = true;
 
-            const position = new THREE.Vector3();
-            const quaternion = new THREE.Quaternion();
-            const scale = new THREE.Vector3(1, 1, 1);
-            const matrix = new THREE.Matrix4();
+            const done = () => {
+              updating = false;
 
-            const numGrassPositions = grassPositions.length / 3;
-            for (let i = 0; i < numGrassPositions; i++) {
-              const baseIndex = i * 3;
-              position.set(
-                grassPositions[baseIndex + 0],
-                grassPositions[baseIndex + 1],
-                grassPositions[baseIndex + 2]
-              );
-              quaternion.setFromAxisAngle(upVector, Math.random() * Math.PI * 2);
-              matrix.compose(position, quaternion, scale);
-              const geometry = grassGeometry
-                .clone()
-                .applyMatrix(matrix);
-              const newPositions = geometry.getAttribute('position').array;
-              positions.set(newPositions, attributeIndex);
-              const newColors = geometry.getAttribute('color').array;
-              colors.set(newColors, attributeIndex);
+              if (updateQueued) {
+                updateQueued = false;
 
-              attributeIndex += newPositions.length;
-            }
-            const geometry = new THREE.BufferGeometry();
-            geometry.addAttribute('position', new THREE.BufferAttribute(new Float32Array(positions.buffer, positions.byteOffset, attributeIndex), 3));
-            geometry.addAttribute('color', new THREE.BufferAttribute(new Float32Array(colors.buffer, colors.byteOffset, attributeIndex), 3));
-
-            const material = grassMaterial;
-
-            const mesh = new THREE.Mesh(geometry, material);
-            // mesh.position.y = 0.5;
-            // mesh.updateMatrixWorld();
-            // mesh.frustumCulled = false;
-            mesh.drawMode = THREE.TriangleStripDrawMode;
-
-            mesh.destroy = () => {
-              geometry.dispose();
+                tryGrassChunkUpdate();
+              }
             };
 
-            return mesh;
-          };
+            _requestRefreshGrassChunks()
+              .then(done)
+              .catch(err => {
+                console.warn(err);
 
-          const _requestRefreshGrassChunks = () => {
-            const {hmd} = pose.getStatus();
-            const {worldPosition: hmdPosition} = hmd;
-            const {added, removed} = chunker.update(hmdPosition.x, hmdPosition.z);
-
-            const addedPromises = added.map(chunk => {
-              const {x, z} = chunk;
-
-              return _requestGrassGenerate(x, z)
-                .then(grassPositions => {
-                  const grassMesh = _makeGrassMesh(grassPositions);
-                  scene.add(grassMesh);
-
-                  chunk.data = grassMesh;
-                });
-            });
-            return Promise.all(addedPromises)
-              .then(() => {
-                removed.forEach(chunk => {
-                  const {data: grassMesh} = chunk;
-                  scene.remove(grassMesh);
-                  grassMesh.destroy();
-                });
-              })
-          };
-
-          return _requestRefreshGrassChunks()
-            .then(() => {
-              let updating = false;
-              let updateQueued = false;
-              const tryGrassChunkUpdate = () => {
-                if (!updating) {
-                  updating = true;
-
-                  const done = () => {
-                    updating = false;
-
-                    if (updateQueued) {
-                      updateQueued = false;
-
-                      tryGrassChunkUpdate();
-                    }
-                  };
-
-                  _requestRefreshGrassChunks()
-                    .then(done)
-                    .catch(err => {
-                      console.warn(err);
-
-                      done();
-                    });
-                } else {
-                  updateQueued = true;
-                }
-              };
-
-              const _update = () => {
-                tryGrassChunkUpdate();
-              };
-              render.on('update', _update);
-
-              this._cleanup = () => {
-                scene.remove(treeMesh);
-
-                grassMaterial.dispose();
-
-                render.removeListener('update', _update);
-              };
-            });
+                done();
+              });
+          } else {
+            updateQueued = true;
           }
-        });
+        };
+
+        const _update = () => {
+          tryGrassChunkUpdate();
+        };
+        render.on('update', _update);
+
+        this._cleanup = () => {
+          // XXX remove old grass meshes here
+
+          grassMaterial.dispose();
+
+          render.removeListener('update', _update);
+        };
+      });
   }
 
   unmount() {
