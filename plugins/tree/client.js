c4ce3a2328b9e7180d66da72160678566e82b0fa
Adrian Biedrzycki <a@modules.io>
2017-06-30 10:37:51 -0700

    Procedural generation plugins cleanup

--- plugins/tree/client.js
+++ plugins/tree/client.js
@@ -5,8 +5,6 @@ const {
 } = require('./lib/constants/constants');
 const protocolUtils = require('./lib/utils/protocol-utils');
 
-const NUM_POSITIONS = 2 * 1000 * 1000;
-
 class Tree {
   constructor(archae) {
     this._archae = archae;
@@ -88,61 +86,6 @@ class Tree {
       range: 2,
       useLods: false,
     });
-
-    const _makeTreeMesh = treePositions => {
-      const positions = new Float32Array(NUM_POSITIONS * 3);
-      // const normals = new Float32Array(NUM_POSITIONS * 3);
-      const colors = new Float32Array(NUM_POSITIONS * 3);
-      const indices = new Uint32Array(NUM_POSITIONS * 3);
-      let attributeIndex = 0;
-      let indexIndex = 0;
-
-      const position = new THREE.Vector3();
-      const quaternion = new THREE.Quaternion();
-      const scale = new THREE.Vector3(1, 1, 1);
-      const matrix = new THREE.Matrix4();
-
-      const numTreePositions = treePositions.length / 3;
-      for (let i = 0; i < numTreePositions; i++) {
-        const baseIndex = i * 3;
-        position.set(
-          treePositions[baseIndex + 0],
-          treePositions[baseIndex + 1],
-          treePositions[baseIndex + 2]
-        );
-        quaternion.setFromAxisAngle(upVector, Math.random() * Math.PI * 2);
-        matrix.compose(position, quaternion, scale);
-        const geometry = treeGeometry
-          .clone()
-          .applyMatrix(matrix);
-        const newPositions = geometry.getAttribute('position').array;
-        positions.set(newPositions, attributeIndex);
-        /* const newNormals = geometry.getAttribute('normal').array;
-        normals.set(newNormals, attributeIndex); */
-        const newColors = geometry.getAttribute('color').array;
-        colors.set(newColors, attributeIndex);
-        const newIndices = geometry.index.array;
-        _copyIndices(newIndices, indices, indexIndex, attributeIndex / 3);
-
-        attributeIndex += newPositions.length;
-        indexIndex += newIndices.length;
-      }
-      const geometry = new THREE.BufferGeometry();
-      geometry.addAttribute('position', new THREE.BufferAttribute(new Float32Array(positions.buffer, positions.byteOffset, attributeIndex), 3));
-      // geometry.addAttribute('normal', new THREE.BufferAttribute(new Float32Array(normals.buffer, normals.byteOffset, attributeIndex), 3));
-      geometry.addAttribute('color', new THREE.BufferAttribute(new Float32Array(colors.buffer, colors.byteOffset, attributeIndex), 3));
-      geometry.setIndex(new THREE.BufferAttribute(new Uint32Array(indices.buffer, indices.byteOffset, indexIndex), 1));
-
-      const material = treeMaterial;
-      const mesh = new THREE.Mesh(geometry, material);
-      // mesh.frustumCulled = false;
-
-      mesh.destroy = () => {
-        geometry.dispose();
-      };
-
-      return mesh;
-    };
 
     const _requestRefreshTreeChunks = () => {
       const {hmd} = pose.getStatus();
