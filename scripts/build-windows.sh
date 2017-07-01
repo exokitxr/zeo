386def81ed47dea43bac4b983e0fcc0675fcf9c7
Adrian Biedrzycki <a@modules.io>
2017-07-01 07:59:36 -0700

    Windows build script clean up

--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+VERSION="8.1.3";
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 pushd "$DIR"/..;
 
-curl https://nodejs.org/dist/v8.1.3/node-v8.1.3-win-x64.zip >node.zip
+rm -Rf node_modules installed data/mod-hashes.json 'Zeo VR.lnk' node "node-v$VERSION-win-x64" windows-release.zip
+npm install
+node index.js install
+curl "https://nodejs.org/dist/v$VERSION/node-v$VERSION-win-x64.zip" >node.zip
 unzip node.zip
 rm node.zip
-mv node-v8.1.2-win-x64 node
+mv "node-v$VERSION-win-x64" node
 cp node/node_modules/npm/bin/npm.cmd node/
-./index.js install
 touch installed/trust-mod-hashes.json
 cp 'scripts/lib/windows/Zeo VR.lnk' .
 zip -r windows-release.zip . -x /.git* -x /data*
 rm -R node
-rm -R installed
 rm 'Zeo VR.lnk'
 
 popd;
