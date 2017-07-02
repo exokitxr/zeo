e6182ba9085b30790915c7201d397d9ad53063b3
Adrian Biedrzycki <a@modules.io>
2017-07-01 20:59:02 -0700

    Port grass plugin to new worker model

--- plugins/grass/lib/constants/constants.js
+++ plugins/grass/lib/constants/constants.js
@@ -2,8 +2,11 @@ const NUM_CELLS = 32;
 const OVERSCAN = 1;
 const NUM_CELLS_OVERSCAN = NUM_CELLS + OVERSCAN;
 
+const DEFAULT_SEED = 'a';
+
 module.exports = {
   NUM_CELLS,
   OVERSCAN,
   NUM_CELLS_OVERSCAN,
+  DEFAULT_SEED,
 };
