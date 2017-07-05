2432bf3d99b4dd90afd65077d1b7fac01bb2a7c3
Adrian Biedrzycki <a@modules.io>
2017-07-05 10:30:34 -0700

    Notification engine alignment cleanup

--- core/engines/notification/lib/render/notification.js
+++ core/engines/notification/lib/render/notification.js
@@ -5,7 +5,7 @@ const {
 
 const getHudSrc = text => {
   return `<div style="display: flex; width: ${WIDTH}px; height: ${HEIGHT}px; justify-content: center; align-items: center;">
-    <div style="display: flex; margin-top: auto; padding: 10px 20px; font-family: Consolas, 'Liberation Mono', Menlo, Courier, monospace; background-color: #000; color: #FFF; font-size: 30px; font-weight: 600; text-align: right;">${text}</div>
+    <div style="display: flex; margin-top: auto; padding: 10px 20px; font-family: Consolas, 'Liberation Mono', Menlo, Courier, monospace; background-color: #000; color: #FFF; font-size: 30px; font-weight: 600; text-align: center;">${text}</div>
   </div>`;
 };
 
