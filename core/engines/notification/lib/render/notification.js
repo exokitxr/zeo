6d1d9a698e1d6cff0717e6aabe76c64414884848
Adrian Biedrzycki <a@modules.io>
2017-07-05 10:10:32 -0700

    Darken notifications background for visibility

--- core/engines/notification/lib/render/notification.js
+++ core/engines/notification/lib/render/notification.js
@@ -4,8 +4,8 @@ const {
 } = require('../constants/notification');
 
 const getHudSrc = text => {
-  return `<div style="display: flex; width: ${WIDTH}px; height: ${HEIGHT}px; color: #FFF; justify-content: center; align-items: center;">
-    <div style="display: flex; margin-top: auto; font-family: Consolas, 'Liberation Mono', Menlo, Courier, monospace; font-size: 30px; font-weight: 600; text-align: right;">${text}</div>
+  return `<div style="display: flex; width: ${WIDTH}px; height: ${HEIGHT}px; justify-content: center; align-items: center;">
+    <div style="display: flex; margin-top: auto; padding: 10px 20px; font-family: Consolas, 'Liberation Mono', Menlo, Courier, monospace; background-color: #000; color: #FFF; font-size: 30px; font-weight: 600; text-align: right;">${text}</div>
   </div>`;
 };
 
