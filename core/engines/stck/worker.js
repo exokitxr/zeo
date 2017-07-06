5c06ed5e82fe81a8b6faf1567132582c9a20ce85
Adrian Biedrzycki <a@modules.io>
2017-07-06 16:15:27 -0700

    Stck engine cleanup

--- core/engines/stck/worker.js
+++ core/engines/stck/worker.js
@@ -211,36 +211,3 @@ self.onmessage = e => {
 };
 
 const _arrayEquals = (a, b) => a[0] === b[0] && a[1] === b[1] && a[2] === b[2];
-const _base64ToArray = sB64Enc => {
-  function _b64ToUint6(nChr) {
-    return nChr > 64 && nChr < 91 ?
-        nChr - 65
-      : nChr > 96 && nChr < 123 ?
-        nChr - 71
-      : nChr > 47 && nChr < 58 ?
-        nChr + 4
-      : nChr === 43 ?
-        62
-      : nChr === 47 ?
-        63
-      :
-        0;
-  }
-
-  const nInLen = sB64Enc.length;
-  const nOutLen = ((nInLen * 3) >> 2) - +(sB64Enc[sB64Enc.length - 1] === '=') - +(sB64Enc[sB64Enc.length - 2] === '=');
-  const taBytes = new Uint8Array(nOutLen);
-
-  for (let nMod3, nMod4, nUint24 = 0, nOutIdx = 0, nInIdx = 0; nInIdx < nInLen; nInIdx++) {
-    nMod4 = nInIdx & 3;
-    nUint24 |= _b64ToUint6(sB64Enc.charCodeAt(nInIdx)) << 18 - 6 * nMod4;
-    if (nMod4 === 3 || nInLen - nInIdx === 1) {
-      for (nMod3 = 0; nMod3 < 3 && nOutIdx < nOutLen; nMod3++, nOutIdx++) {
-        taBytes[nOutIdx] = nUint24 >>> (16 >>> nMod3 & 24) & 255;
-      }
-      nUint24 = 0;
-    }
-  }
-
-  return taBytes;
-};
