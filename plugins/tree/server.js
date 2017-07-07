78041e5c8542c36ac4dd1d10837cf045142f8114
Adrian Biedrzycki <a@modules.io>
2017-07-07 12:14:02 -0700

    Add missing tree plugin server

---
+++ plugins/tree/server.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+class Tree {
+  constructor(archae) {
+    this._archae = archae;
+  }
+
+  mount() {
+    const {_archae: archae} = this;
+    const {express, app} = archae.getCore();
+
+    const treeSfxStatic = express.static(path.join(__dirname, 'lib/sfx'));
+    function serveTreeSfx(req, res, next) {
+      treeSfxStatic(req, res, next);
+    }
+    app.use('/archae/tree/sfx', serveTreeSfx);
+
+    this._cleanup = () => {
+      function removeMiddlewares(route, i, routes) {
+        if (route.handle.name === 'serveTreeSfx') {
+          routes.splice(i, 1);
+        }
+        if (route.route) {
+          route.route.stack.forEach(removeMiddlewares);
+        }
+      }
+      app._router.stack.forEach(removeMiddlewares);
+    };
+  }
+
+  unmount() {
+    this._cleanup();
+  }
+}
+
+module.exports = Tree;
