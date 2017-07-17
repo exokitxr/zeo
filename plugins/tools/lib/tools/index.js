3231d6a8425f400cb7e53e943b4c13e762dfb57c
Adrian Biedrzycki <a@modules.io>
2017-07-17 09:01:19 -0700

    Add new initial mirror tool

--- plugins/tools/lib/tools/index.js
+++ plugins/tools/lib/tools/index.js
@@ -1,9 +1,11 @@
 const hammer = require('./hammer');
 const torch = require('./torch');
+const mirror = require('./mirror');
 
 const toolsLib = ({archae}) => [
   hammer,
   torch,
+  mirror,
 ].map(toolLib => toolLib({archae}));
 
 module.exports = toolsLib;
