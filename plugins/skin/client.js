45ab850b17c2f517bbd4cb3cff934e23a8bafa43
Adrian Biedrzycki <a@modules.io>
2017-07-18 14:38:07 -0700

    Major skin plugin rendering work

--- plugins/skin/client.js
+++ plugins/skin/client.js
@@ -4,8 +4,8 @@ const SIDES = ['left', 'right'];
 
 class Skin {
   mount() {
-    const {three, elements, pose, render, player, utils: {skin: skinUtils}} = zeo;
-    const {THREE, camera} = three;
+    const {three, pose, render, player, utils: {skin: skinUtils}} = zeo;
+    const {THREE, scene, camera, renderer} = three;
     const {skin} = skinUtils;
 
     let live = true;
@@ -55,24 +55,33 @@ class Skin {
         if (live) {
           const _makeMesh = (playerId = null) => {
             const mesh = skin(skinImg);
-            mesh.rotation.order = camera.rotation.order; // XXX should go in skin-js
             mesh.playerId = playerId;
             return mesh;
           };
 
           const localMesh = _makeMesh();
-          entityObject.add(localMesh);
+          localMesh.onBeforeRender = () => {
+            if (renderer.getRecursing()) {
+              localMesh.material.uniforms.headVisible.value = 1;
+            }
+          };
+          localMesh.onAfterRender = () => {
+            if (renderer.getRecursing()) {
+              localMesh.material.uniforms.headVisible.value = 0;
+            }
+          };
+          scene.add(localMesh);
           meshes.push(localMesh);
 
           const _addMesh = playerId => {
             const mesh = _makeMesh(playerId);
-            entityObject.add(mesh);
+            scene.add(mesh);
             meshes.push(mesh);
           };
           const _removeMesh = playerId => {
             const meshIndex = meshes.findIndex(mesh => mesh.playerId === playerId);
             const mesh = meshes[meshIndex];
-            entityObject.remove(mesh);
+            scene.remove(mesh);
             meshes.splice(meshIndex, 1);
           };
           const statuses = player.getRemoteStatuses();
@@ -124,51 +133,41 @@ class Skin {
             const {position: hmdPosition, rotation: hmdRotation} = hmdStatus;
 
             const hmdEuler = new THREE.Euler().setFromQuaternion(hmdRotation, camera.rotation.order);
-            const playerEuler = new THREE.Euler.setFromQuaternion(mesh.quaternion, camera.rotation.order);
+            const playerEuler = new THREE.Euler().setFromQuaternion(mesh.quaternion, camera.rotation.order);
             const angleDiff = _angleDiff(hmdEuler.y, playerEuler.y);
             const angleDiffAbs = Math.abs(angleDiff);
             if (angleDiffAbs > Math.PI / 2) {
               playerEuler.y += (angleDiffAbs - (Math.PI / 2)) * (angleDiff < 0 ? 1 : -1);
               mesh.quaternion.setFromEuler(playerEuler);
-              mesh.updateMatrixWorld();
             }
 
-            const eyesOffset = new THREE.Vector3(0, 1.5, 0); // XXX should go in skin-js
             mesh.position.copy(hmdPosition)
-              .sub(eyesOffset);
-            const playerQuaternionInverse = mesh.getWorldQuaternion().inverse();
-            const headQuaternion = hmdRotation.clone()
-              .premultiply(playerQuaternionInverse);
+              .sub(mesh.getEyeOffset());
+            mesh.updateMatrixWorld();
+            const playerQuaternionInverse = mesh.quaternion.clone().inverse();
+            const headQuaternion = hmdRotation.clone().inverse()
+              .premultiply(mesh.quaternion);
             mesh.material.uniforms.headRotation.value.set(headQuaternion.x, headQuaternion.y, headQuaternion.z, headQuaternion.w);
 
-            const armOffsets = { // XXX should go in skin-js
-              left: new THREE.Vector3(0.3, 1, 0),
-              right: new THREE.Vector3(-0.3, 1, 0)
-            };
-            const armRotations = {
-              left: mesh.material.uniforms.leftArmRotation,
-              right: mesh.material.uniforms.rightArmRotation,
-            };
-            for (let i = 0; i < SIDES.length; i++) {
+            /* for (let i = 0; i < SIDES.length; i++) {
               const side = SIDES[i];
               const controllerStatus = controllersStatus[side];
               const {position: controllerPosition, rotation: controllerRotation} = controllerStatus;
-              const armOffset = armOffsets[side];
+              const armOffset = mesh[side === 'left' ? 'getLeftArmOffset' : 'getRightArmOffset']();
               const upVector = new THREE.Vector3(0, side === 'left' ? -1 : 1, 0).applyQuaternion(controllerRotation);
               const rotationMatrix = new THREE.Matrix4().lookAt(
+                mesh.position.clone().add(armOffset.applyQuaternion(mesh.quaternion)),
                 controllerPosition,
-                mesh.getWorldPosition()
-                  .add(armOffset.clone().applyQuaternion(mesh.getWorldQuaternion())),
                 upVector
               );
-              const localArmQuaternion = new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0, 1, 0), new THREE.Vector3(0, 0, -1));
+              const localArmQuaternion = new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0, -1, 0), new THREE.Vector3(0, 0, -1));
               const armQuaternion = new THREE.Quaternion()
                 .setFromRotationMatrix(rotationMatrix)
                 .multiply(localArmQuaternion)
                 .premultiply(playerQuaternionInverse);
-              const armRotation = armRotations[side];
+              const armRotation = mesh.material.uniforms[side === 'left' ? 'leftArmRotation' : 'rightArmRotation'];
               armRotation.value.set(armQuaternion.x, armQuaternion.y, armQuaternion.z, armQuaternion.w);
-            }
+            } */
           };
           const _update = () => {
             for (let i = 0; i < meshes.length; i++) {
@@ -179,16 +178,14 @@ class Skin {
           render.on('update', _update);
           _update();
 
-          const _renderStart = () => {
-            const {head} = localMesh; // XXX needs to be supported in skin-js
-            head.visible = false;
+          const _updateStart = () => {
+            localMesh.material.uniforms.headVisible.value = 0;
           };
-          render.on('renderStart', _renderStart);
-          const _renderEnd = () => {
-            const {head} = localMesh;
-            head.visible = true;
+          render.on('updateStart', _updateStart);
+          const _updateEnd = () => {
+            localMesh.material.uniforms.headVisible.value = 1;
           };
-          render.on('renderEnd', _renderEnd);
+          render.on('updateEnd', _updateEnd);
 
           const _playerEnter = ({id}) => {
             _addMesh(id);
@@ -202,13 +199,12 @@ class Skin {
           this._cleanup = () => {
             for (let i = 0; i < meshes.length; i++) {
               const mesh = meshes[i];
-              entityObject.remove(mesh);
+              scene.remove(mesh);
             }
-            meshes.length = 0;
 
             render.removeListener('update', _update);
-            render.removeListener('renderStart', _renderStart);
-            render.removeListener('renderEnd', _renderEnd);
+            render.removeListener('updateStart', _updateStart);
+            render.removeListener('updateEnd', _updateEnd);
 
             player.removeListener('playerEnter', _playerEnter);
             player.removeListener('playerLeave', _playerLeave);
