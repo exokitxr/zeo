2cf00f34e9018ef7365f7c2b0c3cc779aaf26593
Adrian Biedrzycki <a@modules.io>
2017-07-20 22:30:02 -0700

    Fix turtle model

--- plugins/zoo/client.js
+++ plugins/zoo/client.js
@@ -80,8 +80,8 @@ class Zoo {
     const {THREE, scene} = three;
 
     const _makeDebugBoxMesh = i => {
-      const boxCenter = new THREE.Vector3(0 * (i !== undefined ? (i === 0 ? -1 : 1) : 1), 0, -1.5);
-      const boxSize = new THREE.Vector3(16, 30, 25);
+      const boxCenter = new THREE.Vector3(0 * (i !== undefined ? (i === 0 ? -1 : 1) : 1), 0, 0);
+      const boxSize = new THREE.Vector3(2.9, 3.2, 3.7);
       return new THREE.Mesh(
         new THREE.BoxBufferGeometry(boxSize.x, boxSize.y, boxSize.z).applyMatrix(new THREE.Matrix4().makeTranslation(
           boxCenter.x, boxCenter.y, boxCenter.z
@@ -252,9 +252,9 @@ class Zoo {
       'skunk',
       'smallbird',
       /* 'spider',
-      'swamplurker',
+      'swamplurker', */
       'turtle',
-      'trilobite', */
+      /* 'trilobite', */
       'velociraptor',
       /* 'villager',
       'walker',
