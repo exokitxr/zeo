afb118ebaa0f5626269a5220b6b06b12ad16f5d4
Adrian Biedrzycki <a@modules.io>
2017-07-21 12:41:54 -0700

    Modernize skunk model

--- plugins/zoo/client.js
+++ plugins/zoo/client.js
@@ -85,8 +85,8 @@ class Zoo {
     };
 
     const _makeDebugBoxMesh = i => {
-      const boxCenter = new THREE.Vector3(0 * (i !== undefined ? (i === 0 ? -1 : 1) : 1), 2, -2.5);
-      const boxSize = new THREE.Vector3(2, 2, 2);
+      const boxCenter = new THREE.Vector3(0 * (i !== undefined ? (i === 0 ? -1 : 1) : 1), 3.5, -2);
+      const boxSize = new THREE.Vector3(2, 2, 3);
       return new THREE.Mesh(
         new THREE.BoxBufferGeometry(boxSize.x, boxSize.y, boxSize.z).applyMatrix(new THREE.Matrix4().makeTranslation(
           boxCenter.x, boxCenter.y, boxCenter.z
@@ -268,11 +268,11 @@ class Zoo {
       'pterodactyl', */
       // 'rat',
       // 'sheep',
-      // 'skunk',
+      'skunk',
       // 'smallbird',
       /* 'spider',
       'swamplurker', */
-      'turtle',
+      // 'turtle',
       /* 'trilobite', */
       // 'velociraptor',
       /* 'villager',
