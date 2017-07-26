a160d5a5188d7f1bcd8f170de2c7815ab9d0d450
Adrian Biedrzycki <a@modules.io>
2017-07-26 12:19:43 -0700

    Clean up biolumi engine chromium remote port selection to work with multiple servers started in parallel

--- scripts/test-http.sh
+++ scripts/test-http.sh
@@ -4,15 +4,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 pushd "$DIR"/../
 
-#./index.js server \
-#  port=7777 \
-#  siteUrl='https://test.zeovr.io:8080' \
-#  homeUrl='http://127.0.0.1:8081' \
-#  vridUrl='https://test.zeovr.io:8080' \
-#  crdsUrl='http://test.zeovr.io:9999' \
-#  dataDirectory='data/servers/server_one/data' dataDirectorySrc='defaults/data' \
-#  cryptoDirectory='data/servers/server_one/crypto' cryptoDirectorySrc='crypto' \
-#  installDirectory='data/servers/server_one/installed' &
+./index.js server \
+  port=7777 \
+  siteUrl='https://test.zeovr.io:8080' \
+  homeUrl='http://127.0.0.1:8081' \
+  vridUrl='https://test.zeovr.io:8080' \
+  crdsUrl='http://test.zeovr.io:9999' \
+  dataDirectory='data/servers/server_one/data' dataDirectorySrc='defaults/data' \
+  cryptoDirectory='data/servers/server_one/crypto' cryptoDirectorySrc='crypto' \
+  installDirectory='data/servers/server_one/installed' &
 ./index.js server \
   port=7778 \
   siteUrl='https://test.zeovr.io:8080' \
