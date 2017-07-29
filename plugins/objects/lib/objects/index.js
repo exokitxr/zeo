0608aa27586a91aaf2b04ebc08534a6fcab8e2c6
Adrian Biedrzycki <a@modules.io>
2017-07-29 01:23:47 -0700

    Move torch to objects plugin

--- plugins/objects/lib/objects/index.js
+++ plugins/objects/lib/objects/index.js
@@ -1,7 +1,9 @@
 const craftingTable = require('./crafting-table');
+const torch = require('./torch');
 
 const objectsLib = options => [
   craftingTable,
+  torch,
 ].map(objectLib => objectLib(options));
 
 module.exports = objectsLib;
--- plugins/tools/lib/tools/index.js
+++ plugins/tools/lib/tools/index.js
@@ -1,14 +1,12 @@
 const sword = require('./sword');
 const bow = require('./bow');
 const hammer = require('./hammer');
-const torch = require('./torch');
 const mirror = require('./mirror');
 
 const toolsLib = options => [
   sword,
   bow,
   hammer,
-  torch,
   mirror,
 ].map(toolLib => toolLib(options));
 
