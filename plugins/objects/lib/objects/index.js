8dce7a471342d6de7133cca31145dd41ff4cb0a9
Adrian Biedrzycki <a@modules.io>
2017-07-30 12:36:21 -0700

    Add objects plugin wood object generator

--- plugins/objects/lib/objects/index.js
+++ plugins/objects/lib/objects/index.js
@@ -1,8 +1,10 @@
+const wood = require('./wood');
 const stone = require('./stone');
 const craftingTable = require('./crafting-table');
 const torch = require('./torch');
 
 const objectsLib = options => [
+  wood,
   stone,
   craftingTable,
   torch,
