8bfd2bd2c15456fb86625764d449e3b7d8d6dc88
Adrian Biedrzycki <a@modules.io>
2017-07-30 10:46:40 -0700

    Add initial stone object to objects plugin

--- plugins/objects/lib/objects/index.js
+++ plugins/objects/lib/objects/index.js
@@ -1,7 +1,9 @@
+const stone = require('./stone');
 const craftingTable = require('./crafting-table');
 const torch = require('./torch');
 
 const objectsLib = options => [
+  stone,
   craftingTable,
   torch,
 ].map(objectLib => objectLib(options));
