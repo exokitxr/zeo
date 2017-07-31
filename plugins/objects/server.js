5c5ceecf6eff1beda5dfa566c502eb039b24c227
Adrian Biedrzycki <a@modules.io>
2017-07-31 15:14:35 -0700

    Bugfix objects plugin zeode file load/save

--- plugins/objects/server.js
+++ plugins/objects/server.js
@@ -90,58 +90,9 @@ class Objects {
         }
       });
     });
-    const _getObjects = zde => {
-      generateApi.registerNoise('elevation', { // XXX move these into the objects lib
-        seed: DEFAULT_SEED,
-        frequency: 0.002,
-        octaves: 8,
-      });
-      generateApi.registerNoise('grass', {
-        seed: DEFAULT_SEED,
-        frequency: 0.1,
-        octaves: 4,
-      });
-      generateApi.registerNoise('tree', {
-        seed: DEFAULT_SEED,
-        frequency: 0.1,
-        octaves: 4,
-      });
-      generateApi.registerNoise('items', {
-        seed: DEFAULT_SEED + '2',
-        frequency: 0.1,
-        octaves: 4,
-      });
-
-      const objectApi = {
-        THREE,
-        registerGenerator(name, fn) {
-          const n = murmur(name);
-          const generator = [n, fn];
-          generators.push(generator);
-
-          for (let i = 0; i < zde.chunks.length; i++) {
-            _generateChunkWithGenerator(zde.chunks[i], generator);
-          }
-        },
-      };
-
-      return Promise.all(
-        objectsLib(objectApi)
-          .map(makeObject => makeObject())
-      );
-    };
 
     return _getZeode()
-    .then(zde => {
-      return Promise.all([
-        Promise.resolve(zde),
-        _getObjects(zde),
-      ]);
-    })
-      .then(([
-        zde,
-        objectCleanups,
-      ])=> {
+      .then(zde => {
         const _writeFileData = (data, byteOffset) => new Promise((accept, reject) => {
           const ws = fs.createWriteStream(zeodeDataPath, {
             flags: 'r+',
@@ -171,131 +122,167 @@ class Objects {
             });
         });
 
-        const objectsImgStatic = express.static(path.join(__dirname, 'lib', 'img'));
-        function serveObjectsImg(req, res, next) {
-          objectsImgStatic(req, res, next);
-        }
-        app.use('/archae/objects/img', serveObjectsImg);
+        const _getObjects = () => {
+          generateApi.registerNoise('elevation', { // XXX move these into the objects lib
+            seed: DEFAULT_SEED,
+            frequency: 0.002,
+            octaves: 8,
+          });
+          generateApi.registerNoise('grass', {
+            seed: DEFAULT_SEED,
+            frequency: 0.1,
+            octaves: 4,
+          });
+          generateApi.registerNoise('tree', {
+            seed: DEFAULT_SEED,
+            frequency: 0.1,
+            octaves: 4,
+          });
+          generateApi.registerNoise('items', {
+            seed: DEFAULT_SEED + '2',
+            frequency: 0.1,
+            octaves: 4,
+          });
 
-        app.get('/archae/objects/chunks', (req, res, next) => {
-          const {query: {x: xs, z: zs}} = req;
-          const x = parseInt(xs, 10);
-          const z = parseInt(zs, 10);
+          const objectApi = {
+            THREE,
+            registerGenerator(name, fn) {
+              const n = murmur(name);
+              const generator = [n, fn];
+              generators.push(generator);
 
-          if (!isNaN(x) && !isNaN(z)) {
-            let chunk = zde.getChunk(x, z);
-            if (!chunk) {
-              chunk = zde.makeChunk(x, z);
-              _generateChunk(chunk);
-            }
-            const uint32Buffer = chunk.getBuffer();
-            const buffer = new Buffer(uint32Buffer.buffer, uint32Buffer.byteOffset, uint32Buffer.byteLength);
-            res.type('application/octet-stream');
-            res.send(buffer);
-          } else {
-            res.status(400);
-            res.send();
-          }
-        });
-
-        const connections = [];
-        const _connection = c => {
-          const {url} = c.upgradeReq;
-
-          if (url === '/archae/objectsWs') {
-            const _broadcast = m => {
-              for (let i = 0; i < connections.length; i++) {
-                const connection = connections[i];
-                if (connection.readyState === ws.OPEN && connection !== c) {
-                  connection.send(m);
+              if (zde.chunks.length > 0) {
+                for (let i = 0; i < zde.chunks.length; i++) {
+                  _generateChunkWithGenerator(zde.chunks[i], generator);
                 }
+                _saveChunks();
+              }
+            },
+          };
+
+          return Promise.all(
+            objectsLib(objectApi)
+              .map(makeObject => makeObject())
+          );
+        };
+
+        return _getObjects()
+          .then(objectCleanups => {
+            const objectsImgStatic = express.static(path.join(__dirname, 'lib', 'img'));
+            function serveObjectsImg(req, res, next) {
+              objectsImgStatic(req, res, next);
+            }
+            app.use('/archae/objects/img', serveObjectsImg);
+
+            app.get('/archae/objects/chunks', (req, res, next) => {
+              const {query: {x: xs, z: zs}} = req;
+              const x = parseInt(xs, 10);
+              const z = parseInt(zs, 10);
+
+              if (!isNaN(x) && !isNaN(z)) {
+                let chunk = zde.getChunk(x, z);
+                if (!chunk) {
+                  chunk = zde.makeChunk(x, z);
+                  _generateChunk(chunk);
+                  _saveChunks();
+                }
+                const uint32Buffer = chunk.getBuffer();
+                const buffer = new Buffer(uint32Buffer.buffer, uint32Buffer.byteOffset, uint32Buffer.byteLength);
+                res.type('application/octet-stream');
+                res.send(buffer);
+              } else {
+                res.status(400);
+                res.send();
+              }
+            });
+
+            const connections = [];
+            const _connection = c => {
+              const {url} = c.upgradeReq;
+
+              if (url === '/archae/objectsWs') {
+                const _broadcast = m => {
+                  for (let i = 0; i < connections.length; i++) {
+                    const connection = connections[i];
+                    if (connection.readyState === ws.OPEN && connection !== c) {
+                      connection.send(m);
+                    }
+                  }
+                };
+
+                c.on('message', msg => {
+                  const m = JSON.parse(msg);
+                  const {method} = m;
+
+                  if (method === 'addObject') {
+                    const {args: {x, z, n, matrix}} = m;
+
+                    let chunk = zde.getChunk(x, z);
+                    if (!chunk) {
+                      chunk = zde.makeChunk(x, z);
+                      _generateChunk(chunk);
+                    }
+                    chunk.addObject(n, matrix);
+
+                    _saveChunks();
+
+                    _broadcast({
+                      type: 'addObject',
+                      x,
+                      z,
+                      n,
+                      matrix,
+                    });
+                  } else if (method === 'removeObject') {
+                    const {args: {x, z, index}} = m;
+
+                    const chunk = zde.getChunk(x, z);
+                    if (chunk) {
+                      chunk.removeObject(index);
+
+                      _saveChunks();
+
+                      _broadcast({
+                        type: 'removeObject',
+                        index,
+                      });
+                    }
+                  } else {
+                    console.warn('objects server got unknown method:', JSON.stringify(method));
+                  }
+                });
+                c.on('close', () => {
+                  connections.splice(connections.indexOf(c), 1);
+                });
+
+                connections.push(c);
               }
             };
+            wss.on('connection', _connection);
 
-            c.on('message', msg => {
-              const m = JSON.parse(msg);
-              const {method} = m;
-
-              /*if (method === 'getChunk') {
-                const {args: {x, z}} = m;
-
-                let chunk = zde.getChunk(x, z);
-                if (!chunk) {
-                  chunk = zde.makeChunk(x, z);
-                  _generateChunk(chunk);
-                }
-                c.send(JSON.stringify({
-                  type: 'response',
-                }));
-                c.send(chunk.getBuffer());
-              } else */if (method === 'addObject') {
-                const {args: {x, z, n, matrix}} = m;
-
-                let chunk = zde.getChunk(x, z);
-                if (!chunk) {
-                  chunk = zde.makeChunk(x, z);
-                  _generateChunk(chunk);
-                }
-                chunk.addObject(n, matrix);
-
-                _saveChunks();
-
-                _broadcast({
-                  type: 'addObject',
-                  x,
-                  z,
-                  n,
-                  matrix,
-                });
-              } else if (method === 'removeObject') {
-                const {args: {x, z, index}} = m;
-
-                const chunk = zde.getChunk(x, z);
-                if (chunk) {
-                  chunk.removeObject(index);
-
-                  _saveChunks();
-
-                  _broadcast({
-                    type: 'removeObject',
-                    index,
-                  });
-                }
-              } else {
-                console.warn('objects server got unknown method:', JSON.stringify(method));
+            this._cleanup = () => {
+              for (let i = 0; i < objectCleanups.length; i++) {
+                const objectCleanup = objectCleanups[i];
+                objectCleanup();
               }
-            });
-            c.on('close', () => {
-              connections.splice(connections.indexOf(c), 1);
-            });
 
-            connections.push(c);
-          }
-        };
-        wss.on('connection', _connection);
+              function removeMiddlewares(route, i, routes) {
+                if (route.handle.name === 'serveObjectsImg') {
+                  routes.splice(i, 1);
+                }
+                if (route.route) {
+                  route.route.stack.forEach(removeMiddlewares);
+                }
+              }
+              app._router.stack.forEach(removeMiddlewares);
 
-        this._cleanup = () => {
-          for (let i = 0; i < objectCleanups.length; i++) {
-            const objectCleanup = objectCleanups[i];
-            objectCleanup();
-          }
-
-          function removeMiddlewares(route, i, routes) {
-            if (route.handle.name === 'serveObjectsImg') {
-              routes.splice(i, 1);
-            }
-            if (route.route) {
-              route.route.stack.forEach(removeMiddlewares);
-            }
-          }
-          app._router.stack.forEach(removeMiddlewares);
-
-          for (let i = 0; i < connections.length; i++) {
-            const c = connections[i];
-            c.close();
-          }
-          wss.removeListener('connection', _connection);
-        };
+              for (let i = 0; i < connections.length; i++) {
+                const c = connections[i];
+                c.close();
+              }
+              wss.removeListener('connection', _connection);
+            };
+          });
       });
   }
 
