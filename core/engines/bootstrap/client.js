283611fadc0f767d5840668ee4a1fc2a89d02625
Adrian Biedrzycki <a@modules.io>
2017-08-02 07:03:24 -0700

    Bootstrap engine log recent servers to cookie

--- core/engines/bootstrap/client.js
+++ core/engines/bootstrap/client.js
@@ -1,4 +1,4 @@
-const FACES = ['top', 'bottom', 'left', 'right', 'front', 'back'];
+const NUM_SERVERS = 5;
 
 class Bootstrap {
   constructor(archae) {
@@ -7,7 +7,19 @@ class Bootstrap {
 
   mount() {
     const {_archae: archae} = this;
-    const {metadata: {site: {url: siteUrl}, server: {enabled: serverEnabled}}} = archae;
+    const {
+      metadata: {
+        server: {
+          url: serverUrl,
+        },
+        site: {
+          url: siteUrl,
+        },
+        vrid: {
+          url: vridUrl,
+        },
+      },
+    } = archae;
 
     let live = true;
     this._cleanup = () => {
@@ -16,6 +28,63 @@ class Bootstrap {
 
     const initialUrl = document.location.href;
     const initialPath = document.location.protocol + '//' + document.location.host + document.location.pathname;
+
+    const _resJson = res => {
+      if (res.status >= 200 && res.status < 300) {
+        return res.json();
+      } else {
+        return Promise.reject({
+          status: res.status,
+          stack: 'API returned invalid status code: ' + res.status,
+        });
+      }
+    };
+    const _resBlob = res => {
+      if (res.status >= 200 && res.status < 300) {
+        return res.blob();
+      } else {
+        return Promise.reject({
+          status: res.status,
+          stack: 'API returned invalid status code: ' + res.status,
+        });
+      }
+    };
+    const _requestKickServer = () => fetch(`${vridUrl}/id/api/cookie/servers`, {
+      credentials: 'include',
+    })
+      .then(_resJson)
+      .then(servers => {
+        if (!Array.isArray(servers)) {
+          servers = [];
+        }
+        let server = servers.find(server => server.url === serverUrl);
+        if (!server) {
+          server = {
+            url: serverUrl,
+            timestamp: 0,
+          };
+          servers.push(server);
+        }
+        server.timestamp = Date.now();
+        servers.sort((a, b) => b.timestamp - a.timestamp);
+        servers.length = Math.min(servers.length, NUM_SERVERS);
+
+        return fetch(`${vridUrl}/id/api/cookie/servers`, {
+          method: 'POST',
+          headers: (() => {
+            const headers = new Headers();
+            headers.append('Content-Type', 'application/json');
+            return headers;
+          })(),
+          body: JSON.stringify(servers),
+          credentials: 'include',
+        })
+          .then(_resBlob);
+      });
+    _requestKickServer()
+      .catch(err => {
+        console.warn(err);
+      });
 
     return archae.requestPlugins([
       '/core/utils/js-utils',
