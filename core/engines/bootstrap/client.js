f6c7f3a1e84857f9ca4c00b2296b21062c7dba1b
Adrian Biedrzycki <a@modules.io>
2017-08-04 18:34:05 -0700

    Small bootstrap engine cleanup

--- core/engines/bootstrap/client.js
+++ core/engines/bootstrap/client.js
@@ -211,28 +211,5 @@ const _getQueryVariable = (url, variable) => {
   }
   return null;
 };
-const _debounce = fn => {
-  let running = false;
-  let queued = false;
-
-  const _go = () => {
-    if (!running) {
-      running = true;
-
-      fn(() => {
-        running = false;
-
-        if (queued) {
-          queued = false;
-
-          _go();
-        }
-      });
-    } else {
-      queued = true;
-    }
-  };
-  return _go;
-};
 
 module.exports = Bootstrap;
