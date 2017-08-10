7c2c1c25769aa046411a471aaf2b6f79e4f0e7b2
Adrian Biedrzycki <a@modules.io>
2017-08-10 14:10:58 -0700

    Major simplification to fs engine

--- core/engines/fs/server.js
+++ core/engines/fs/server.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const fs = require('fs');
-const child_process = require('child_process');
 
 const mkdirp = require('mkdirp');
 
@@ -26,8 +25,8 @@ class Fs {
     });
 
     const fsPath = path.join(dirname, dataDirectory, 'fs');
-    const _ensureFsDirectory = () => new Promise((accept, reject) => {
-      mkdirp(fsPath, err => {
+    const _ensureDirectory = p => new Promise((accept, reject) => {
+      mkdirp(p, err => {
         if (!err) {
           accept();
         } else {
@@ -36,167 +35,36 @@ class Fs {
       });
     });
 
-    return _ensureFsDirectory()
+    return _ensureDirectory(fsPath)
       .then(() => {
         if (live) {
-          const libStatic = express.static(path.join(__dirname, 'lib'));
-          function serveLibStatic(req, res, next) {
-            libStatic(req, res, next);
-          }
-          app.use('/archae/fs/lib', serveLibStatic);
-
           const fsStatic = express.static(fsPath);
-          function serveFsStatic(req, res, next) {
-            const dirname = req.params[0];
-            let filePath = req.params[1];
-
-            const _serveSingle = () => {
-              req.url = '/' + dirname + filePath;
-
-              res.set('Content-Disposition', 'attachment; filename="' + path.basename(filePath) + '"');
-
-              fsStatic(req, res, next);
-            };
-
-            if (!/^\/?$/.test(filePath)) {
-              _serveSingle();
-            } else {
-              const fullPath = path.join(fsPath, dirname);
-
-              const _serveMultiple = () => {
-                res.type('application/octet-stream');
-                res.set('Content-Disposition', 'attachment; filename="' + dirname + '.zip"');
-
-                const zipProcess = child_process.spawn('zip', [
-                  '-r',
-                  '-',
-                  '.',
-                ], {
-                  cwd: fullPath,
-                });
-
-                zipProcess.stdout.pipe(res);
-                zipProcess.stderr.pipe(process.stderr);
-                zipProcess.on('exit', code => {
-                  if (code !== 0) {
-                    res.status(500);
-                    res.send('zip returned non-zero status code: ' + code);
-                  }
-                });
-              };
-
-              fs.readdir(fullPath, (err, files) => {
-                if (!err) {
-                  if (files.length === 0) {
-                    _serveSingle();
-                  } else if (files.length === 1) {
-                    const file = files[0];
-
-                    fs.lstat(path.join(fullPath, file), (err, stats) => {
-                      if (!err) {
-                        if (stats.isFile()) {
-                          filePath = '/' + file;
-
-                          _serveSingle();
-                        } else if (stats.isDirectory()) {
-                          _serveMultiple();
-                        } else {
-                          res.status(404);
-                          res.send();
-                        }
-                      } else {
-                        res.status(404);
-                        res.send();
-                      }
-                    });
-                  } else {
-                    _serveMultiple();
-                  }
-                } else {
-                  res.status(404);
-                  res.send();
-                }
-              });
-            }
+          function serveFsDownload(req, res, next) {
+            req.url = '/' + req.params[0];
+            // res.set('Content-Disposition', 'attachment; filename="' + path.basename(filePath) + '"');
+            fsStatic(req, res, next);
           }
-          app.get(/^\/fs\/([^\/]+)((?:\/.*)?)$/, serveFsStatic);
+          app.get(/^\/fs\/([^\/]+)$/, serveFsDownload);
 
           function serveFsUpload(req, res, next) {
-            const dirname = req.params[0];
-            const filePath = req.params[1];
-
-            const fullPath = path.join(fsPath, dirname, filePath);
-            mkdirp(path.dirname(fullPath), err => {
-              const _ok = () => {
-                res.send();
-              };
-              const _error = err => {
-                res.status(500);
-                res.send(err.stack);
-              };
-
-              if (!err) {
-                fs.lstat(fullPath, err => {
-                  const _write = () => {
-                    const start = (() => {
-                      const rangeString = req.get('range');
-
-                      if (rangeString) {
-                        const match = rangeString.match(/^bytes=([0-9]+)-$/);
-
-                        if (match) {
-                          const rangeValue = parseInt(match[1], 10);
-
-                          if (!isNaN(rangeValue)) {
-                            return rangeValue;
-                          } else {
-                            return 0;
-                          }
-                        } else {
-                          return 0;
-                        }
-                      } else {
-                        return 0;
-                      }
-                    })();
-
-                    const ws = fs.createWriteStream(fullPath, {
-                      flags: 'r+',
-                      start: start,
-                    });
-
-                    req.pipe(ws);
-
-                    ws.on('finish', _ok);
-                    ws.on('error', _error);
-                  };
-
-                  if (!err) {
-                    _write();
-                  } else if (err.code === 'ENOENT') {
-                    fs.writeFile(fullPath, '', err => {
-                      if (!err) {
-                        _write();
-                      } else {
-                        _error(err);
-                      }
-                    });
-                  } else {
-                    _error(err);
-                  }
-                });
-              } else {
-                _error(err);
-              }
+            const ws = fs.createWriteStream(path.join(fsPath, req.params[0]));
+            req.pipe(ws);
+            ws.on('finish', err => {
+              res.send();
+            });
+            ws.on('error', err => {
+              res.status(500);
+              res.json({
+                error: err.stack,
+              });
             });
           }
-          app.put(/^\/fs\/([^\/]+)(\/.+)$/, serveFsUpload);
+          app.put(/^\/fs\/([^\/]+)$/, serveFsUpload);
 
           cleanups.push(() => {
             function removeMiddlewares(route, i, routes) {
               if (
-                route.handle.name === 'serveLibStatic' ||
-                route.handle.name === 'serveFsStatic' ||
+                route.handle.name === 'serveFsDownload' ||
                 route.handle.name === 'serveFsUpload'
               ) {
                 routes.splice(i, 1);
@@ -208,7 +76,7 @@ class Fs {
             app._router.stack.forEach(removeMiddlewares);
           });
 
-          class FsFile {
+          /* class FsFile {
             constructor(dirname, pathname) {
               this.dirname = dirname;
               this.pathname = pathname;
@@ -256,7 +124,7 @@ class Fs {
 
           return {
             makeFile: _makeFile,
-          };
+          }; */
         }
       });
   }
