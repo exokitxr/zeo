7ee766de3879e52ec54a5319ca0428df260c2c7d
Adrian Biedrzycki <a@modules.io>
2017-08-12 03:19:10 -0700

    Bugfix missing mirror object constant

--- plugins/objects/lib/objects/client/mirror.js
+++ plugins/objects/lib/objects/client/mirror.js
@@ -1,6 +1,11 @@
 const ThreeExtraMirror = require('../../three-extra/Mirror');
 
 const HEIGHTFIELD_PLUGIN = 'plugins-heightfield';
+const DEFAULT_MATRIX = [
+  0, 0, 0,
+  0, 0, 0, 1,
+  1, 1, 1,
+];
 
 const dataSymbol = Symbol();
 
