568ffff43e8395f60823fd5281c90fe253c9bc7e
Adrian Biedrzycki <a@modules.io>
2017-08-16 08:33:24 -0700

    Move objects plugin to get elevation from heightfield plugin

--- plugins/objects/lib/objects/server/stick.js
+++ plugins/objects/lib/objects/server/stick.js
@@ -5,8 +5,6 @@ const stick = objectApi => {
       const localQuaternion = new generateApi.THREE.Quaternion();
       const localEuler = new generateApi.THREE.Euler();
 
-      const _getElevation = (ox, oz, x, z) => (-0.3 + Math.pow(generateApi.getNoise('elevation', ox, oz, x, z), 0.5)) * 64;
-
       const itemProbability = 0.05;
 
       for (let dz = 0; dz < generateApi.NUM_CELLS_OVERSCAN; dz++) {
@@ -14,7 +12,7 @@ const stick = objectApi => {
           const v = generateApi.getNoise('items', chunk.x, chunk.z, dx, dz);
 
           if (v < itemProbability && (generateApi.getHash(String(v)) % 2) === 1) {
-            const elevation = _getElevation(chunk.x, chunk.z, dx, dz);
+            const elevation = chunk.heightfield[(dx + (dz * generateApi.NUM_CELLS_OVERSCAN)) * 8];
 
             const ax = (chunk.x * generateApi.NUM_CELLS) + dx;
             const az = (chunk.z * generateApi.NUM_CELLS) + dz;
