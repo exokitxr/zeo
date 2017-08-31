ace070e616895eeb25abb202169af3ed12ed54dd
Adrian Biedrzycki <a@modules.io>
2017-08-31 05:25:40 -0700

    Add house stairs

--- plugins/objects/lib/objects/server/house.js
+++ plugins/objects/lib/objects/server/house.js
@@ -7,6 +7,8 @@ const {
 
 const {three: {THREE}, utils: {image: {jimp}}} = zeo;
 
+const NUM_POSITIONS = 10 * 1024;
+
 const _parseSpec = s => {
   const lines = s.split('\n');
   const legend = (() => {
@@ -120,6 +122,70 @@ const house = objectApi => {
       })();
       objectApi.registerGeometry('house', houseGeometry);
 
+      const stairsGeometry = (() => {
+        const woodUvs = objectApi.getUv('house');
+        const uvWidth = woodUvs[2] - woodUvs[0];
+        const uvHeight = woodUvs[3] - woodUvs[1];
+
+        const geometry = (() => {
+          const frontGeometry = new THREE.BoxBufferGeometry(1, 1/4, 1)
+            .applyMatrix(new THREE.Matrix4().makeTranslation(0, 1/4/2, 0));
+
+          const middleGeometry = new THREE.BoxBufferGeometry(1, 1/4, 2/3)
+            .applyMatrix(new THREE.Matrix4().makeTranslation(0, 1/4/2 + 1/4, -1/3/2));
+
+          const backGeometry = new THREE.BoxBufferGeometry(1, 1/4, 1/3)
+            .applyMatrix(new THREE.Matrix4().makeTranslation(0, 1/4/2 + 2/4, -2/3/2));
+
+          const _copyIndices = (src, dst, startIndexIndex, startAttributeIndex) => {
+            for (let i = 0; i < src.length; i++) {
+              dst[startIndexIndex + i] = src[i] + startAttributeIndex;
+            }
+          };
+
+          const geometry = new THREE.BufferGeometry();
+          const positions = new Float32Array(NUM_POSITIONS);
+          const normals = new Float32Array(NUM_POSITIONS);
+          const uvs = new Float32Array(NUM_POSITIONS);
+          const indices = new Uint16Array(NUM_POSITIONS);
+          let attributeIndex = 0;
+          let uvIndex = 0;
+          let indexIndex = 0;
+          [
+            frontGeometry,
+            middleGeometry,
+            backGeometry,
+          ].forEach(newGeometry => {
+            const newPositions = newGeometry.getAttribute('position').array;
+            positions.set(newPositions, attributeIndex);
+            const newNormals = newGeometry.getAttribute('normal').array;
+            normals.set(newNormals, attributeIndex);
+            const newUvs = newGeometry.getAttribute('uv').array;
+            uvs.set(newUvs, uvIndex);
+            const newIndices = newGeometry.index.array;
+            _copyIndices(newIndices, indices, indexIndex, attributeIndex / 3);
+
+            attributeIndex += newPositions.length;
+            uvIndex += newUvs.length;
+            indexIndex += newIndices.length;
+          });
+          geometry.addAttribute('position', new THREE.BufferAttribute(new Float32Array(positions.buffer, positions.byteOffset, attributeIndex), 3));
+          geometry.addAttribute('normal', new THREE.BufferAttribute(new Float32Array(normals.buffer, normals.byteOffset, attributeIndex), 3));
+          geometry.addAttribute('uv', new THREE.BufferAttribute(new Float32Array(uvs.buffer, uvs.byteOffset, uvIndex), 2));
+          geometry.setIndex(new THREE.BufferAttribute(new Uint16Array(indices.buffer, indices.byteOffset, indexIndex), 1));
+          return geometry;
+        })();
+        const uvs = geometry.getAttribute('uv').array;
+        const numUvs = uvs.length / 2;
+        for (let i = 0; i < numUvs; i++) {
+          uvs[i * 2 + 0] = woodUvs[0] + (uvs[i * 2 + 0] * uvWidth);
+          uvs[i * 2 + 1] = (woodUvs[1] + uvHeight) - (uvs[i * 2 + 1] * uvHeight);
+        }
+
+        return geometry;
+      })();
+      objectApi.registerGeometry('stairs', stairsGeometry);
+
       objectApi.registerGenerator('house', chunk => {
         if (chunk.x === 0 && chunk.z === -1) {
           const elevation = chunk.heightfield[(0 + (0 * NUM_CELLS_OVERSCAN)) * 8];
@@ -135,10 +201,19 @@ const house = objectApi => {
               const row = layer[z];
 
               for (let x = 0; x < layer.length; x++) {
-                const block = row[x];
+                const col = row[x];
 
-                if (block) {
-                  objectApi.addObject(chunk, 'house', localVector.set(x, elevation + y, z), localQuaternion, 0);
+                if (col) {
+                  const block = (() => {
+                    if (col === 'cobblestone-stairs') {
+                      return 'stairs';
+                    } else if (col === 'torch') {
+                      return 'torch';
+                    } else {
+                      return 'house';
+                    }
+                  })();
+                  objectApi.addObject(chunk, block, localVector.set(x, elevation + y, z), localQuaternion, 0);
                 }
               }
             }
