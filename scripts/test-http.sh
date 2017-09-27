ad204222d3cc507c79a353e91b9cfd48df867ded
Adrian Biedrzycki <a@modules.io>
2017-09-27 12:39:58 -0700

    Clean up test script

--- scripts/test-http.sh
+++ scripts/test-http.sh
@@ -4,24 +4,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 pushd "$DIR"/../
 
-./index.js server \
-  port=7777 \
+node --inspect=0.0.0.0:9229 ./index.js server \
+  port=8000 \
   siteUrl='https://test.zeovr.io:8080' \
   homeUrl='http://127.0.0.1:8081' \
   vridUrl='https://test.zeovr.io:8080' \
   crdsUrl='http://test.zeovr.io:9999' \
-  dataDirectory='data/servers/server_one/data' dataDirectorySrc='defaults/data' \
-  cryptoDirectory='data/servers/server_one/crypto' cryptoDirectorySrc='crypto' \
-  installDirectory='data/servers/server_one/installed' &
-./index.js server \
-  port=7778 \
-  siteUrl='https://test.zeovr.io:8080' \
-  homeUrl='http://127.0.0.1:8081' \
-  vridUrl='https://test.zeovr.io:8080' \
-  crdsUrl='http://test.zeovr.io:9999' \
-  dataDirectory='data/servers/server_two/data' dataDirectorySrc='defaults/data' \
-  cryptoDirectory='data/servers/server_two/crypto' cryptoDirectorySrc='crypto' \
-  installDirectory='data/servers/server_two/installed' &
+  dataDirectory='data' dataDirectorySrc='defaults/data' \
+  cryptoDirectory='data/crypto' cryptoDirectorySrc='crypto' \
+  installDirectory='data/installed' &
 
 sleep infinity;
 
