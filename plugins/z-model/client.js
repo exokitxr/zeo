94410fd271d11078aae2f6d2d14ad91ed32845a9
Adrian Biedrzycki <a@modules.io>
2017-09-30 07:05:46 -0700

    Z-model plugin revert damage

--- plugins/z-model/client.js
+++ plugins/z-model/client.js
@@ -1,50 +1,103 @@
-const Zlib = require('./inflate.min.js');
-const FBXLoader = require('./FBXLoader');
-
-// const URL = `https://cdn.rawgit.com/mrdoob/three.js/3bdf3cde63d48b897474927a4bd6e1876e104f73/examples/models/fbx/xsi_man_skinning.fbx`;
-// const URL = `/archae/models/models/xsi_man_skinning.fbx`;
-
 class Model {
   mount() {
-    const {three: {THREE, scene}, input, elements, hands} = zeo;
-
-    const THREEFBXLoader = FBXLoader({THREE, Zlib});
-    const manager = new THREE.LoadingManager();
+    const {three: {THREE}, elements} = zeo;
 
     const modelEntity = {
-      attributes: {},
+      attributes: {
+        position: {
+          type: 'matrix',
+          value: [
+            0, 0, 0,
+            0, 0, 0, 1,
+            1, 1, 1,
+          ],
+        },
+        src: {
+          type: 'file',
+          value: 'https://cdn.rawgit.com/modulesio/zeo-data/29412380b29e98b18c746a373bdb73aeff59e27a/models/cloud/cloud.json',
+        },
+      },
       entityAddedCallback(entityElement) {
-        const _triggerdown = e => {
-          const {side} = e;
-          const grabbable = hands.getGrabbedGrabbable(side);
+        const entityApi = entityElement.getEntityApi();
+        const entityObject = entityElement.getObject();
 
-          if (grabbable && grabbable.type === 'file') {
-            const loader = new THREEFBXLoader(manager);
-            loader.load('/archae/fs/hash/' + grabbable.value, object => {
-              const parent = new THREE.Object3D();
-              parent.position.set(0, 64, 0);
-              parent.quaternion.setFromUnitVectors(
-                new THREE.Vector3(0, 0, 1),
-                new THREE.Vector3(0, 1, 0)
-              );
-              parent.add(object);
-              parent.updateMatrixWorld();
-              scene.add(parent);
-            }, progress => {
-              // console.log('progress', progress);
-            }, err => {
-              console.warn(err);
-            });
+        entityApi.position = null;
+        entityApi.mesh = null;
+
+        entityApi._cancelRequest = null;
+
+        entityApi._updateMesh = () => {
+          const {mesh, position} = entityApi;
+
+          if (mesh && position) {
+            mesh.position.set(position[0], position[1], position[2]);
+            mesh.quaternion.set(position[3], position[4], position[5], position[6]);
+            mesh.scale.set(position[7], position[8], position[9]);
+            mesh.updateMatrixWorld();
           }
         };
-        input.on('triggerdown', _triggerdown);
 
-        entityElement._cleanup = () => {
-          input.removeListener('triggerdown', _triggerdown);
+        entityApi._cleanup = () => {
+          const {mesh, _cancelRequest: cancelRequest} = entityApi;
+          if (mesh) {
+            entityObject.remove(mesh);
+          }
+          if (cancelRequest) {
+            cancelRequest();
+          }
         };
       },
       entityRemovedCallback(entityElement) {
-        entityElement._cleanup();
+        const entityApi = entityElement.getEntityApi();
+
+        entityApi._cleanup();
+      },
+      entityAttributeValueChangedCallback(entityElement, name, oldValue, newValue) {
+        const entityApi = entityElement.getEntityApi();
+        const entityObject = entityElement.getObject();
+
+        switch (name) {
+          case 'position': {
+            entityApi.position = newValue;
+
+            entityApi._updateMesh();
+
+            break;
+          }
+          case 'src': {
+            const {mesh: oldMesh, _cancelRequest: cancelRequest} = entityApi;
+            if (oldMesh) {
+              entityObject.remove(oldMesh);
+              this.mesh = null;
+            }
+            if (cancelRequest) {
+              cancelRequest();
+            }
+
+            let live = true;
+            entityApi._cancelRequest = () => {
+              live = false;
+            };
+
+            const file = newValue;
+            file.read({type: 'model'})
+              .then(mesh => {
+                if (live) {
+                  entityObject.add(mesh);
+                  entityApi.mesh = mesh;
+
+                  entityApi._updateMesh();
+
+                  entityApi._cancelRequest = null;
+                }
+              })
+              .catch(err => {
+                console.warn('failed to load model', err);
+              });
+
+            break;
+          }
+        }
       },
     }
     elements.registerEntity(this, modelEntity);
