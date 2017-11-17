a2a591b091cba6c04ea16a4cb43c4647b52dd587
Adrian Biedrzycki <a@modules.io>
2017-11-16 22:58:55 -0800

    Add node-webvr build process

---
+++ scripts/lib/windows/node-webvr/build.cmd
@@ -0,0 +1,8 @@
+set npm_config_target=1.7.9
+set npm_config_arch=x64
+set npm_config_target_arch=x64
+set npm_config_disturl=https://atom.io/download/electron
+set npm_config_runtime=electron
+set npm_config_build_from_source=true
+
+npm install --no-package-lock
