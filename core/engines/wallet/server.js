f1afb688c702203ac41ceae09ddc4dc7c46ee97a
Adrian Biedrzycki <a@modules.io>
2017-12-04 19:54:13 -0800

    Wallet engine analytics require debugging

--- core/engines/wallet/server.js
+++ core/engines/wallet/server.js
@@ -10,105 +10,119 @@ class Wallet {
     const {_archae: archae} = this;
     const {ws, wss} = archae.getCore();
 
-    class AssetInstance {
-      constructor(id, type, assetId, name, ext, path, attributes, icon, n, physics, matrix, visible, open) {
-        this.id = id;
-        this.type = type;
-        this.assetId = assetId;
-        this.name = name;
-        this.ext = ext;
-        this.path = path;
-        this.attributes = attributes;
-        this.icon = icon;
-        this.n = n;
-        this.physics = physics;
-        this.matrix = matrix;
-        this.visible = visible;
-        this.open = open;
-      }
-    }
+    let live = true;
+    this._cleanup = () => {
+      live = false;
+    };
 
-    const assetInstances = [];
-
-    const connections = [];
-    wss.on('connection', (c, {url}) => {
-      if (url === '/archae/walletWs') {
-        const _init = () => {
-          c.send(JSON.stringify({
-            type: 'init',
-            args: {
-              assets: assetInstances,
+    return archae.requestPlugins([
+      '/core/engines/analytics',
+    ])
+      .then(([
+        analytics,
+      ]) => {
+        if (live) {
+          class AssetInstance {
+            constructor(id, type, assetId, name, ext, path, attributes, icon, n, physics, matrix, visible, open) {
+              this.id = id;
+              this.type = type;
+              this.assetId = assetId;
+              this.name = name;
+              this.ext = ext;
+              this.path = path;
+              this.attributes = attributes;
+              this.icon = icon;
+              this.n = n;
+              this.physics = physics;
+              this.matrix = matrix;
+              this.visible = visible;
+              this.open = open;
             }
-          }));
-        };
-        _init();
-
-        const _broadcast = m => {
-          for (let i = 0; i < connections.length; i++) {
-            const connection = connections[i];
-            if (connection.readyState === ws.OPEN && connection !== c) {
-              connection.send(m);
-            }
-          };
-        };
-
-        c.on('message', s => {
-          const m = _jsonParse(s);
-          const {method, args} = m;
-
-          if (method === 'addAsset') {
-            const {id, type, assetId, name, ext, path, attributes, icon, n, physics, matrix, visible, open} = args;
-            const assetInstance = new AssetInstance(id, type, assetId, name, ext, path, attributes, icon, n, physics, matrix, visible, open);
-            assetInstances.push(assetInstance);
-
-            _broadcast(JSON.stringify({type: 'addAsset', args: {id, type, assetId, name, ext, path, attributes, icon, n, physics, matrix, visible, open}}));
-
-            analytics.addFile({id});
-          } else if (method === 'removeAsset') {
-            const {id} = args;
-            assetInstances.splice(assetInstances.findIndex(assetInstance => assetInstance.id === id), 1);
-
-            _broadcast(JSON.stringify({type: 'removeAsset', args: {id}}));
-
-            analytics.removeFile({id});
-          } else if (method === 'setVisible') {
-            const {id, visible} = args;
-            const assetInstance = assetInstances.find(assetInstance => assetInstance.id === id);
-
-            if (assetInstance) {
-              assetInstance.visible = visible;
-
-              _broadcast(JSON.stringify({type: 'setVisible', args: {id, visible}}));
-            }
-          } else if (method === 'setOpen') {
-            const {id, open} = args;
-            const assetInstance = assetInstances.find(assetInstance => assetInstance.id === id);
-
-            if (assetInstance) {
-              assetInstance.open = open;
-
-              _broadcast(JSON.stringify({type: 'setOpen', args: {id, open}}));
-            }
-          } else if (method === 'setPhysics') {
-            const {id, physics} = args;
-            const assetInstance = assetInstances.find(assetInstance => assetInstance.id === id);
-
-            if (assetInstance) {
-              assetInstance.physics = physics;
-
-              _broadcast(JSON.stringify({type: 'setPhysics', args: {id, physics}}));
-            }
-          } else {
-            console.warn('no such method:' + JSON.stringify(method));
           }
-        });
-        c.on('close', () => {
-          connections.splice(connections.indexOf(c), 1);
-        });
 
-        connections.push(c);
-      }
-    });
+          const assetInstances = [];
+
+          const connections = [];
+          wss.on('connection', (c, {url}) => {
+            if (url === '/archae/walletWs') {
+              const _init = () => {
+                c.send(JSON.stringify({
+                  type: 'init',
+                  args: {
+                    assets: assetInstances,
+                  }
+                }));
+              };
+              _init();
+
+              const _broadcast = m => {
+                for (let i = 0; i < connections.length; i++) {
+                  const connection = connections[i];
+                  if (connection.readyState === ws.OPEN && connection !== c) {
+                    connection.send(m);
+                  }
+                };
+              };
+
+              c.on('message', s => {
+                const m = _jsonParse(s);
+                const {method, args} = m;
+
+                if (method === 'addAsset') {
+                  const {id, type, assetId, name, ext, path, attributes, icon, n, physics, matrix, visible, open} = args;
+                  const assetInstance = new AssetInstance(id, type, assetId, name, ext, path, attributes, icon, n, physics, matrix, visible, open);
+                  assetInstances.push(assetInstance);
+
+                  _broadcast(JSON.stringify({type: 'addAsset', args: {id, type, assetId, name, ext, path, attributes, icon, n, physics, matrix, visible, open}}));
+
+                  analytics.addFile({id});
+                } else if (method === 'removeAsset') {
+                  const {id} = args;
+                  assetInstances.splice(assetInstances.findIndex(assetInstance => assetInstance.id === id), 1);
+
+                  _broadcast(JSON.stringify({type: 'removeAsset', args: {id}}));
+
+                  analytics.removeFile({id});
+                } else if (method === 'setVisible') {
+                  const {id, visible} = args;
+                  const assetInstance = assetInstances.find(assetInstance => assetInstance.id === id);
+
+                  if (assetInstance) {
+                    assetInstance.visible = visible;
+
+                    _broadcast(JSON.stringify({type: 'setVisible', args: {id, visible}}));
+                  }
+                } else if (method === 'setOpen') {
+                  const {id, open} = args;
+                  const assetInstance = assetInstances.find(assetInstance => assetInstance.id === id);
+
+                  if (assetInstance) {
+                    assetInstance.open = open;
+
+                    _broadcast(JSON.stringify({type: 'setOpen', args: {id, open}}));
+                  }
+                } else if (method === 'setPhysics') {
+                  const {id, physics} = args;
+                  const assetInstance = assetInstances.find(assetInstance => assetInstance.id === id);
+
+                  if (assetInstance) {
+                    assetInstance.physics = physics;
+
+                    _broadcast(JSON.stringify({type: 'setPhysics', args: {id, physics}}));
+                  }
+                } else {
+                  console.warn('no such method:' + JSON.stringify(method));
+                }
+              });
+              c.on('close', () => {
+                connections.splice(connections.indexOf(c), 1);
+              });
+
+              connections.push(c);
+            }
+          });
+        }
+      });
   }
 
   unmount() {
