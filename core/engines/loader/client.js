fc3794631195450b1a5ecd064998d8df612af521
Adrian Biedrzycki <a@modules.io>
2017-12-18 21:42:42 -0800

    Loader engine add request loaded plugin api

--- core/engines/loader/client.js
+++ core/engines/loader/client.js
@@ -7,51 +7,108 @@ class Loader {
     const {_archae: archae} = this;
     const {metadata: {offline}} = archae;
 
-    class LoaderApi {
-      requestPlugin(plugin) {
-        const _preload = () => {
-          if (offline) {
-            window.module = {};
-            return _loadScript(`https://try.zeovr.io/build/${plugin}/${plugin}.js`)
-              .then(() => {
-                window.plugins[plugin] = window.module.exports;
-                window.module = {};
-              });
-          } else {
-            return Promise.resolve();
-          }
-        };
-        return _preload()
-          .then(() => archae.requestPlugin(plugin, {
-            hotload: true,
-          }));
-      }
-
-      removePlugin(plugin) {
-        return archae.removePlugin(plugin);
-      }
-
-      releasePlugin(plugin) {
-        return archae.releasePlugin(plugin);
-      }
-    }
-    const loaderApi = new LoaderApi();
-
-    const _unload = plugin => {
-      loaderApi.releasePlugin(plugin);
-    };
-    archae.on('unload', _unload);
-    const _load = plugin => {
-      loaderApi.requestPlugin(plugin);
-    };
-    archae.on('load', _load);
-
+    let live = true;
     this._cleanup = () => {
-      archae.removeListener('unload', _unload);
-      archae.removeListener('load', _load);
+      live = false;
     };
 
-    return loaderApi;
+    return archae.requestPlugins([
+      '/core/utils/js-utils',
+    ]).then(([
+      jsUtils,
+    ]) => {
+      if (live) {
+        const {events} = jsUtils;
+        const {EventEmitter} = events;
+
+        const pluginApis = {};
+
+        class LoaderApi extends EventEmitter {
+          requestPlugin(plugin) {
+            const _preload = () => {
+              if (offline) {
+                window.module = {};
+                return _loadScript(`https://try.zeovr.io/build/${plugin}/${plugin}.js`)
+                  .then(() => {
+                    window.plugins[plugin] = window.module.exports;
+                    window.module = {};
+                  });
+              } else {
+                return Promise.resolve();
+              }
+            };
+            return _preload()
+              .then(() => archae.requestPlugin(plugin, {
+                hotload: true,
+              }))
+              .then(pluginApi => {
+                const pluginName = plugin.replace(/^(.+?)@.*$/, '$1');
+
+                pluginApis[pluginName] = pluginApi;
+
+                loaderApi.emit('pluginAdded', pluginName, pluginApi);
+              });
+          }
+
+          releasePlugin(plugin) {
+            return archae.releasePlugin(plugin)
+              .then(() => {
+                const pluginName = plugin.replace(/^(.+?)@.*$/, '$1');
+
+                pluginApis[pluginName] = null;
+
+                loaderApi.emit('pluginRemoved', plugin);
+              });
+          }
+
+          removePlugin(plugin) {
+            return archae.removePlugin(plugin);
+          }
+
+          requestLoadedPlugin(plugin, {timeout = 30 * 1000} = {}) {
+            return new Promise((accept, reject) => {
+              const pluginApi = pluginApis[plugin];
+
+              if (pluginApi) {
+                accept(pluginApi);
+              } else {
+                const _pluginAdded = (plugin, pluginApi) => {
+                  clearTimeout(localTimeout);
+
+                  accept(pluginApi);
+                };
+                this.on('pluginAdded', _pluginAdded);
+
+                const localTimeout = setTimeout(() => {
+                  this.removeListener('pluginAdded', _pluginAdded);
+
+                  const err = new Error('timeout out');
+                  err.code = 'ETIMEOUT';
+                  reject(err);
+                }, timeout);
+              }
+            });
+          }
+        }
+        const loaderApi = new LoaderApi();
+
+        const _unload = plugin => {
+          loaderApi.releasePlugin(plugin);
+        };
+        archae.on('unload', _unload);
+        const _load = plugin => {
+          loaderApi.requestPlugin(plugin);
+        };
+        archae.on('load', _load);
+
+        this._cleanup = () => {
+          archae.removeListener('unload', _unload);
+          archae.removeListener('load', _load);
+        };
+
+        return loaderApi;
+      }
+    });
   }
 
   unmount() {
