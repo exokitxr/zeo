c6b12d03901d4c2c83b7b81a7160ca84c452c64b
Adrian Biedrzycki <a@modules.io>
2017-12-21 00:44:13 -0800

    Wallet engine support items save

--- core/engines/wallet/server.js
+++ core/engines/wallet/server.js
@@ -1,5 +1,13 @@
 const events = require('events');
 const {EventEmitter} = events;
+const path = require('path');
+const fs = require('fs');
+
+const mkdirp = require('mkdirp');
+
+const DEFAULT_ITEMS = {
+  items: {},
+};
 
 class Wallet {
   constructor(archae) {
@@ -8,22 +16,73 @@ class Wallet {
 
   mount() {
     const {_archae: archae} = this;
-    const {ws, wss} = archae.getCore();
+    const {ws, wss, dirname, dataDirectory} = archae.getCore();
 
     let live = true;
     this._cleanup = () => {
       live = false;
     };
 
-    return archae.requestPlugins([
-      '/core/engines/multiplayer',
-      '/core/engines/analytics',
+    const worldPath = path.join(dirname, dataDirectory, 'world');
+    const worldItemsJsonPath = path.join(worldPath, 'items.json');
+
+    const _requestFile = (p, defaultValue) => new Promise((accept, reject) => {
+      fs.readFile(p, 'utf8', (err, s) => {
+        if (!err) {
+          const j = JSON.parse(s);
+          accept(j);
+        } else if (err.code === 'ENOENT') {
+          const j = defaultValue;
+          accept(j);
+        } else {
+          reject(err);
+        }
+      });
+    });
+    const _requestItemsJson = () => _requestFile(worldItemsJsonPath, DEFAULT_ITEMS);
+
+    return Promise.all([
+      archae.requestPlugins([
+        '/core/engines/multiplayer',
+        '/core/engines/analytics',
+      ]),
+      _requestItemsJson(),
     ])
       .then(([
-        multiplayer,
-        analytics,
+        [
+          multiplayer,
+          analytics,
+        ],
+        itemsJson,
       ]) => {
         if (live) {
+          const _saveFile = (p, j) => new Promise((accept, reject) => {
+            mkdirp(path.dirname(p), err => {
+              if (!err) {
+                fs.writeFile(p, JSON.stringify(j, null, 2), 'utf8', err => {
+                  if (!err) {
+                    accept();
+                  } else {
+                    reject(err);
+                  }
+                });
+              } else {
+                reject(err);
+              }
+            });
+          });
+          const _saveItems = _debounce(next => {
+            const itemsData = assetInstances.filter(assetSpec => !assetSpec.owner);
+
+            _saveFile(worldItemsJsonPath, itemsData)
+              .then(() => {
+                next();
+              })
+              .catch(err => {
+                console.warn(err);
+              });
+          });
+
           class AssetInstance {
             constructor(assetId, id, name, ext, json, file, n, owner, physics, matrix, visible, open) {
               this.assetId = assetId;
@@ -76,12 +135,16 @@ class Wallet {
 
                   _broadcast(JSON.stringify({type: 'addAsset', args: {assetId, id, name, ext, json, file, n, owner, physics, matrix, visible, open}}));
 
+                  _saveItems();
+
                   analytics.addFile({id});
                 } else if (method === 'removeAsset') {
                   const {assetId} = args;
                   assetInstances.splice(assetInstances.findIndex(assetInstance => assetInstance.assetId === assetId), 1);
 
                   _broadcast(JSON.stringify({type: 'removeAsset', args: {assetId}}));
+
+                  _saveItems();
 
                   analytics.removeFile({assetId});
                 } else if (method === 'setAttribute') {
@@ -93,6 +156,8 @@ class Wallet {
 
                     _broadcast(JSON.stringify({type: 'setAttribute', args: {assetId, name, value}}));
                   }
+
+                  _saveItems();
                 } else if (method === 'setOwner') {
                   const {assetId, owner} = args;
                   const assetInstance = assetInstances.find(assetInstance => assetInstance.assetId === assetId);
@@ -102,6 +167,8 @@ class Wallet {
 
                     _broadcast(JSON.stringify({type: 'setOwner', args: {assetId, owner}}));
                   }
+
+                  _saveItems();
                 } else if (method === 'setVisible') {
                   const {assetId, visible} = args;
                   const assetInstance = assetInstances.find(assetInstance => assetInstance.assetId === assetId);
@@ -111,6 +178,8 @@ class Wallet {
 
                     _broadcast(JSON.stringify({type: 'setVisible', args: {assetId, visible}}));
                   }
+
+                  _saveItems();
                 } else if (method === 'setOpen') {
                   const {assetId, open} = args;
                   const assetInstance = assetInstances.find(assetInstance => assetInstance.assetId === assetId);
@@ -120,6 +189,8 @@ class Wallet {
 
                     _broadcast(JSON.stringify({type: 'setOpen', args: {assetId, open}}));
                   }
+
+                  _saveItems();
                 } else if (method === 'setPhysics') {
                   const {assetId, physics} = args;
                   const assetInstance = assetInstances.find(assetInstance => assetInstance.assetId === assetId);
@@ -129,6 +200,8 @@ class Wallet {
 
                     _broadcast(JSON.stringify({type: 'setPhysics', args: {assetId, physics}}));
                   }
+
+                  _saveItems();
                 } else {
                   console.warn('no such method:' + JSON.stringify(method));
                 }
@@ -191,6 +264,29 @@ const _jsonParse = s => {
   } else {
     return null;
   }
+};
+const _debounce = fn => {
+  let running = false;
+  let queued = false;
+
+  const _go = () => {
+    if (!running) {
+      running = true;
+
+      fn(() => {
+        running = false;
+
+        if (queued) {
+          queued = false;
+
+          _go();
+        }
+      });
+    } else {
+      queued = true;
+    }
+  };
+  return _go;
 };
 
 module.exports = Wallet;
