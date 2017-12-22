744a2a7a099dc4e272129ec84b79592727bcab51
Adrian Biedrzycki <a@modules.io>
2017-12-22 05:51:09 -0800

    Add noTty option to standalone script

--- scripts/standalone.sh
+++ scripts/standalone.sh
@@ -5,4 +5,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 "$DIR"/../index.js server \
   port="$PORT" \
   siteUrl='https://zeovr.io' \
-  vridUrl='https://zeovr.io'
+  vridUrl='https://zeovr.io' \
+  noTty
