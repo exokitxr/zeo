0064c9476dbc18ee501f80ce769ba980b61e534f
Adrian Biedrzycki <a@modules.io>
2018-01-05 02:46:17 -0800

    Inventory engine comments cleanup

--- core/engines/inventory/client.js
+++ core/engines/inventory/client.js
@@ -147,26 +147,17 @@ class Inventory {
       const canvasAspectRatio = canvas.width / canvas.height;
       let renderableHeight, renderableWidth, xStart, yStart;
 
-      // If image's aspect ratio is less than canvas's we fit on height
-      // and place the image centrally along width
-      if(imageAspectRatio < canvasAspectRatio) {
+      if (imageAspectRatio < canvasAspectRatio) {
         renderableHeight = canvas.height;
         renderableWidth = img.width * (renderableHeight / img.height);
         xStart = (canvas.width - renderableWidth) / 2;
         yStart = 0;
-      }
-
-      // If image's aspect ratio is greater than canvas's we fit on width
-      // and place the image centrally along height
-      else if(imageAspectRatio > canvasAspectRatio) {
+      } else if (imageAspectRatio > canvasAspectRatio) {
         renderableWidth = canvas.width
         renderableHeight = img.height * (renderableWidth / img.width);
         xStart = 0;
         yStart = (canvas.height - renderableHeight) / 2;
-      }
-
-      // Happy path - keep aspect ratio
-      else {
+      } else {
         renderableHeight = canvas.height;
         renderableWidth = canvas.width;
         xStart = 0;
